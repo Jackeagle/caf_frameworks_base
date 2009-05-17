09f63a6ff1802571113d412879d20822b3b72e50
Agarwal, Shirish Kumar <shirisha@qualcomm.com>
2009-05-17 10:10:32 +0530

    Wallpaper: Memory leak while changing wallpaper
    Whenever wallpaper is changed, Wallpaper bitmap gets created
    twice by two processes, acore and system_server without
    deallocating the previous bitmaps. Also, callbacks are not being
    unregistered on finishing broadcast by WallpaperService.
    
    CRs-Fixed:182418

--- services/java/com/android/server/WallpaperService.java
+++ services/java/com/android/server/WallpaperService.java
@@ -173,6 +173,7 @@ class WallpaperService extends IWallpaperService.Stub {
         for (int i = 0; i < n; i++) {
             try {
                 mCallbacks.getBroadcastItem(i).onWallpaperChanged();
+                mCallbacks.unregister(mCallbacks.getBroadcastItem(i));
             } catch (RemoteException e) {
 
                 // The RemoteCallbackList will take care of removing
