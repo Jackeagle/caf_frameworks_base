f6ddb7034f6e7cfe1aab7512ce3162686a15b9e2
Alex Yakavenka <c_ayakav@quicinc.com>
2009-10-13 10:49:48 -0700

    Telephony: Load gsm phone first when in Global mode
    CRs-fixed: 213336

--- telephony/java/com/android/internal/telephony/PhoneFactory.java
+++ telephony/java/com/android/internal/telephony/PhoneFactory.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2006 The Android Open Source Project
+ * Copyright (c) 2009, Code Aurora Forum. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -144,7 +145,7 @@ public class PhoneFactory {
             return RILConstants.GSM_PHONE;
 
         case RILConstants.NETWORK_MODE_GLOBAL:
-            return RILConstants.CDMA_PHONE;
+            return RILConstants.GSM_PHONE;
         default:
             return RILConstants.GSM_PHONE;
         }
