4021d22dfa75329bcd40fc4ecebfeefb6747d862
Sri Lakshmi Punuru <c_spunur@qualcomm.com>
2009-11-02 15:19:53 +0530

    Telephony: Fix for a crash when Android UI is shutting down .
    Modified shutdown thread to wait until confirmation of
    RIL_REQUEST_RADIO_POWER with mode 2 is received from qcril,
    before shutting down the Android UI.
    
    CRs-Fixed: 210795

--- telephony/java/com/android/internal/telephony/PhoneBase.java
+++ telephony/java/com/android/internal/telephony/PhoneBase.java
@@ -656,6 +656,11 @@ public abstract class PhoneBase implements Phone {
         return 0;
     }
 
+    /** @hide */
+    public boolean isRilPowerOffComplete() {
+        return false;
+    }
+
     /**
      * Returns the CDMA ERI icon index to display
      */
