def0d2da56201ec0dfdfc7b44d81c9934434756d
Haroun Ansari Mohammed Ansari <c_hmoham@quicinc.com>
2010-03-17 15:25:22 -0700

    telephony : Fix to support CHAP/PAP authentication during CDMA SETUP_DATA_CALL
    Current CDMA SETUP_DATA_CALL supports neither CHAP nor PAP authentication
    preference.Now authentication preference has been changed to CHAP/PAP.
    
    Change-Id: I5de36595830bf4590170feb0b6ea223b6b90d8e3
    CRs-Fixed: 228273

--- telephony/java/com/android/internal/telephony/gsm/PdpConnection.java
+++ telephony/java/com/android/internal/telephony/gsm/PdpConnection.java
@@ -84,9 +84,13 @@ public class PdpConnection extends DataConnection {
         lastFailCause = FailCause.NONE;
         receivedDisconnectReq = false;
 
-        phone.mCM.setupDataCall(Integer.toString(RILConstants.GSM_PHONE),
-                Integer.toString(RILConstants.DATA_PROFILE_DEFAULT), apn.apn, apn.user,
-                apn.password, obtainMessage(EVENT_SETUP_DATA_CONNECTION_DONE));
+        int authType;
+        authType = (apn.user != null) ? RILConstants.SETUP_DATA_AUTH_PAP_CHAP
+                : RILConstants.SETUP_DATA_AUTH_NONE;
+
+        phone.mCM.setupDataCall(Integer.toString(RILConstants.GSM_PHONE), Integer
+                .toString(RILConstants.DATA_PROFILE_DEFAULT), apn.apn, apn.user, apn.password,
+                Integer.toString(authType), obtainMessage(EVENT_SETUP_DATA_CONNECTION_DONE));
     }
 
     private void tearDownData(Message msg) {
