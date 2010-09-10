89e776fb9753cbeb7e737df5d27e4511660f3d37
Vijayadurga Muthusamy <vmuthusa@codeaurora.org>
2010-09-10 16:02:24 -0500

    FM: Proper cleanup of resources
       -Fm APIs have changed in a such a way that it will acquire the FM driver
        and release it on every Enable and Disable of the FMradio. In this way,
        It ensures the proper cleaning up of all the resources.
        It also adds the changes to Fm event listener so that it loops
        through all the available events in the event queue. In this way,
        it ensures all the events are delivered to the application Layer properly.
    
    Change-Id: I30924b7d51a5681a4d0bf2ba32e4225a90b29955
    CRs-Fixed: 250289, 251760

--- core/java/android/hardware/fmradio/FmRxEventListner.java
+++ core/java/android/hardware/fmradio/FmRxEventListner.java
@@ -65,80 +65,90 @@ class FmRxEventListner {
 
                 Log.d(TAG, "Starting listener " + fd);
 
-                while (true) {
+                while ((!Thread.currentThread().isInterrupted())) {
 
-                    byte []buff = new byte[1];
-                    int i = FmReceiverJNI.getBufferNative (fd, buff, EVENT_LISTEN);
-                    Log.d(TAG, "Received <" +buff[0]+ "> event. Int: " + i);
+                    try {
+                        int index = 0;
+                        byte []buff = new byte[64];
+                        int eventCount = FmReceiverJNI.getBufferNative (fd, buff, EVENT_LISTEN);
+                        Log.d(TAG, "Received event. Count: " + eventCount);
 
-                    switch(buff[0]){
+                        for (  index = 0; index < eventCount; index++ ) {
+                            Log.d(TAG, "Received <" +buff[index]+ ">" );
 
-                    case 0:
-                        Log.d(TAG, "Got READY_EVENT");
-                        cb.FmRxEvEnableReceiver();
-                        break;
-                    case 1:
-                        Log.d(TAG, "Got TUNE_EVENT");
-                        cb.FmRxEvRadioTuneStatus(FmReceiverJNI.getFreqNative(fd));
-                        break;
-                    case 2:
-                        Log.d(TAG, "Got SEEK_COMPLETE_EVENT");
-                        cb.FmRxEvSearchComplete(FmReceiverJNI.getFreqNative(fd));
-                        break;
-                    case 3:
-                        Log.d(TAG, "Got SCAN_NEXT_EVENT");
-                        cb.FmRxEvSearchInProgress();
-                        break;
-                    case 4:
-                        Log.d(TAG, "Got RAW_RDS_EVENT");
-                        cb.FmRxEvRdsGroupData();
-                        break;
-                    case 5:
-                        Log.d(TAG, "Got RT_EVENT");
-                        cb.FmRxEvRdsRtInfo();
-                        break;
-                    case 6:
-                        Log.d(TAG, "Got PS_EVENT");
-                        cb.FmRxEvRdsPsInfo();
-                        break;
-                    case 7:
-                        Log.d(TAG, "Got ERROR_EVENT");
-                        break;
-                    case 8:
-                        Log.d(TAG, "Got BELOW_TH_EVENT");
-                        cb.FmRxEvServiceAvailable (false);
-                        break;
-                    case 9:
-                        Log.d(TAG, "Got ABOVE_TH_EVENT");
-                        cb.FmRxEvServiceAvailable(true);
-                        break;
-                    case 10:
-                        Log.d(TAG, "Got STEREO_EVENT");
-                        cb.FmRxEvStereoStatus (true);
-                        break;
-                    case 11:
-                        Log.d(TAG, "Got MONO_EVENT");
-                        cb.FmRxEvStereoStatus (false);
-                        break;
-                    case 12:
-                        Log.d(TAG, "Got RDS_AVAL_EVENT");
-                        cb.FmRxEvRdsLockStatus (true);
-                        break;
-                    case 13:
-                        Log.d(TAG, "Got RDS_NOT_AVAL_EVENT");
-                        cb.FmRxEvRdsLockStatus (false);
-                        break;
-                    case 14:
-                        Log.d(TAG, "Got NEW_SRCH_LIST");
-                        cb.FmRxEvSearchListComplete ();
-                        break;
-                    case 15:
-                        Log.d(TAG, "Got NEW_AF_LIST");
-                        cb.FmRxEvRdsAfInfo();
-                        break;
-                    default:
-                        Log.d(TAG, "Unknown event");
-                        break;
+                            switch(buff[index]){
+
+                            case 0:
+                                Log.d(TAG, "Got READY_EVENT");
+                                cb.FmRxEvEnableReceiver();
+                                break;
+                            case 1:
+                                Log.d(TAG, "Got TUNE_EVENT");
+                                cb.FmRxEvRadioTuneStatus(FmReceiverJNI.getFreqNative(fd));
+                                break;
+                            case 2:
+                                Log.d(TAG, "Got SEEK_COMPLETE_EVENT");
+                                cb.FmRxEvSearchComplete(FmReceiverJNI.getFreqNative(fd));
+                                break;
+                            case 3:
+                                Log.d(TAG, "Got SCAN_NEXT_EVENT");
+                                cb.FmRxEvSearchInProgress();
+                                break;
+                            case 4:
+                                Log.d(TAG, "Got RAW_RDS_EVENT");
+                                cb.FmRxEvRdsGroupData();
+                                break;
+                            case 5:
+                                Log.d(TAG, "Got RT_EVENT");
+                                cb.FmRxEvRdsRtInfo();
+                                break;
+                            case 6:
+                                Log.d(TAG, "Got PS_EVENT");
+                                cb.FmRxEvRdsPsInfo();
+                                break;
+                            case 7:
+                                Log.d(TAG, "Got ERROR_EVENT");
+                                break;
+                            case 8:
+                                Log.d(TAG, "Got BELOW_TH_EVENT");
+                                cb.FmRxEvServiceAvailable (false);
+                            break;
+                            case 9:
+                                Log.d(TAG, "Got ABOVE_TH_EVENT");
+                                cb.FmRxEvServiceAvailable(true);
+                                break;
+                            case 10:
+                                Log.d(TAG, "Got STEREO_EVENT");
+                                cb.FmRxEvStereoStatus (true);
+                                break;
+                            case 11:
+                                Log.d(TAG, "Got MONO_EVENT");
+                                cb.FmRxEvStereoStatus (false);
+                                break;
+                            case 12:
+                                Log.d(TAG, "Got RDS_AVAL_EVENT");
+                                cb.FmRxEvRdsLockStatus (true);
+                                break;
+                            case 13:
+                                Log.d(TAG, "Got RDS_NOT_AVAL_EVENT");
+                                cb.FmRxEvRdsLockStatus (false);
+                                break;
+                            case 14:
+                                Log.d(TAG, "Got NEW_SRCH_LIST");
+                                cb.FmRxEvSearchListComplete ();
+                                break;
+                            case 15:
+                                Log.d(TAG, "Got NEW_AF_LIST");
+                                cb.FmRxEvRdsAfInfo();
+                                break;
+                            default:
+                                Log.d(TAG, "Unknown event");
+                                break;
+                            }
+                        }//end of for
+                    } catch ( Exception ex ) {
+                          Log.d( TAG,  "RunningThread InterruptedException");
+                          Thread.currentThread().interrupt();
                     }
                 }
             }
@@ -147,7 +157,13 @@ class FmRxEventListner {
     }
 
     public void stopListener(){
-        mThread.stop();
+        //mThread.stop();
+        //Thread stop is deprecate API
+        //Interrupt the thread and check for the thread status
+        // and return from the run() method to stop the thread
+        //properly
+        Log.d( TAG,  "stopping the Listener\n");
+        mThread.interrupt();
     }
 
 }
