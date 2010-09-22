34ac6e6338c97572c76bf87eb287eb8adb3fcd25
Vladimir Barinov <vbarinov@odell.mentor.com>
2010-09-22 13:58:05 +0400

    Revert stagefrighthw hack from generic code
    Change-Id: I9b2396f596061dae6224e95e6932d59454844e84
    signed-off-by: batareich@gmail.com

--- media/libstagefright/omx/OMXMaster.cpp
+++ media/libstagefright/omx/OMXMaster.cpp
@@ -83,9 +83,7 @@ void OMXMaster::addPlugin(OMXPluginBase *plugin) {
 
         mPluginByComponentName.add(name8, plugin);
     }
-#if !defined( TARGET_OMAP3) /* So that non-dsp codec plugin is still picked up */
     CHECK_EQ(err, OMX_ErrorNoMore);
-#endif
 }
 
 void OMXMaster::clearPlugins() {
