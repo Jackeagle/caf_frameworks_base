f812b1c1430a8dd6b53685f090547a946bcff074
Jessica Gonzalez <jgaona@codeaurora.org>
2010-10-20 13:02:39 -0700

    frameworks/base: Add rendering statistics to Stagefright
    Rendering statistics can be enabled by setting the property setprop
    persist.debug.sf.statistics to 1. To disable, set the property to 0.
    
    This adds the following logs:
    -Clip type and duration
    -Number of frames read by parser
    -Number of key frames read by parser
    -Number of frames dropped
    -Number of frames rendered
    -First frame latency
    -Seek position and latency after seek
    -Pause position
    -Expected frames per second
    -Real time delta of video if it's ahead or behind
    -Maximum time delta of video if it's ahead or behind
    -Number of occurrences of A/V sync loss
    -Maximum time of A/V sync loss
    
    Conflicts:
    
    	media/libstagefright/MPEG4Extractor.cpp
    
    Change-Id: Ica3ffd0cd169e16efd7351e052053b14e5dea55e

--- media/libstagefright/MPEG4Extractor.cpp
+++ media/libstagefright/MPEG4Extractor.cpp
@@ -38,6 +38,8 @@
 #include <media/stagefright/Utils.h>
 #include <utils/String8.h>
 
+#include <cutils/properties.h>
+
 namespace android {
 
 class MPEG4Source : public MediaSource {
@@ -55,6 +57,8 @@ public:
 
     virtual status_t read(
             MediaBuffer **buffer, const ReadOptions *options = NULL);
+
+    virtual void logTrackStatistics();
 
 protected:
     virtual ~MPEG4Source();
@@ -80,6 +84,11 @@ private:
     bool mWantsNALFragments;
 
     uint8_t *mSrcBuffer;
+
+    //For statistics profiling
+    uint32_t mNumSamplesReadError;
+    bool mStatistics;
+    void logExpectedFrames();
 
     size_t parseNALSize(const uint8_t *data) const;
 
@@ -1418,10 +1427,18 @@ MPEG4Source::MPEG4Source(
       mGroup(NULL),
       mBuffer(NULL),
       mWantsNALFragments(false),
-      mSrcBuffer(NULL) {
+      mSrcBuffer(NULL),
+      mNumSamplesReadError(0) {
     const char *mime;
     bool success = mFormat->findCString(kKeyMIMEType, &mime);
     CHECK(success);
+
+
+    //for statistics profiling
+    char value[PROPERTY_VALUE_MAX];
+    mStatistics = false;
+    property_get("persist.debug.sf.statistics", value, "0");
+    if(atoi(value)) mStatistics = true;
 
     mIsAVC = !strcasecmp(mime, MEDIA_MIMETYPE_VIDEO_AVC);
 
@@ -1439,6 +1456,7 @@ MPEG4Source::MPEG4Source(
         // The number of bytes used to encode the length of a NAL unit.
         mNALLengthSize = 1 + (ptr[4] & 3);
     }
+    if (mStatistics) logExpectedFrames();
 }
 
 MPEG4Source::~MPEG4Source() {
@@ -1569,6 +1587,7 @@ status_t MPEG4Source::read(
                     mCurrentSampleIndex, &offset, &size, &dts);
 
         if (err != OK) {
+            if (mStatistics) mNumSamplesReadError++;
             return err;
         }
 
@@ -1576,6 +1595,7 @@ status_t MPEG4Source::read(
 
         if (err != OK) {
             CHECK_EQ(mBuffer, NULL);
+            if (mStatistics) mNumSamplesReadError++;
             return err;
         }
     }
@@ -1589,6 +1609,7 @@ status_t MPEG4Source::read(
                 mBuffer->release();
                 mBuffer = NULL;
 
+                if (mStatistics) mNumSamplesReadError++;
                 return ERROR_IO;
             }
 
@@ -1622,6 +1643,7 @@ status_t MPEG4Source::read(
             mBuffer->release();
             mBuffer = NULL;
 
+            if (mStatistics) mNumSamplesReadError++;
             return ERROR_MALFORMED;
         }
 
@@ -1653,6 +1675,7 @@ status_t MPEG4Source::read(
             mBuffer->release();
             mBuffer = NULL;
 
+            if (mStatistics) mNumSamplesReadError++;
             return ERROR_IO;
         }
 
@@ -1676,6 +1699,8 @@ status_t MPEG4Source::read(
                 srcOffset += size;
                 ++mCurrentSampleIndex;
                 return ERROR_CORRUPT_NAL;
+                if (mStatistics) mNumSamplesReadError++;
+                return ERROR_MALFORMED;
             }
 
             if (nalLength == 0) {
@@ -1706,6 +1731,29 @@ status_t MPEG4Source::read(
 
         return OK;
     }
+}
+
+void MPEG4Source::logTrackStatistics()
+{
+    LOGW("Total number of samples in track: %lu",mSampleTable->countSamples());
+    LOGW("Number of key samples: %lu",mSampleTable->getNumSyncSamples());
+    LOGW("Number of corrupt samples: %lu",mNumSamplesReadError ?
+           mNumSamplesReadError-1 : mNumSamplesReadError); //last sample reads error for EOS
+}
+
+
+void MPEG4Source::logExpectedFrames()
+{
+    const char *mime;
+    mFormat->findCString(kKeyMIMEType, &mime);
+    int64_t durationUs;
+    getFormat()->findInt64(kKeyDuration, &durationUs);
+    LOGW("=====================================================");
+    LOGW("Mime type: %s",mime);
+    LOGW("Track duration: %lld",durationUs/1000);
+    LOGW("Total number of samples in track: %lu",mSampleTable->countSamples());
+    LOGW("Expected frames per second: %.2f",((float)mSampleTable->countSamples()*1000)/((float)durationUs/1000));
+    LOGW("=====================================================");
 }
 
 bool SniffMPEG4(
