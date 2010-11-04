03506cdd22a3a65ab64441ce0fef0a3f2563e64e
Jessica Gonzalez <jgaona@codeaurora.org>
2010-11-04 16:30:26 -0700

    Framework/Base : Added support for Skin Tone Enhancement
    Conflicts:
    
    	api/current.xml
    
    Change-Id: I6dc4ba9572799428efc34669a0855ad0f9717df4

--- libs/camera/CameraParameters.cpp
+++ libs/camera/CameraParameters.cpp
@@ -180,6 +180,8 @@ const char CameraParameters::KEY_LENSSHADE[] = "lensshade";
 const char CameraParameters::KEY_SUPPORTED_LENSSHADE_MODES[] = "lensshade-values";
 const char CameraParameters::KEY_HISTOGRAM[] = "histogram";
 const char CameraParameters::KEY_SUPPORTED_HISTOGRAM_MODES[] = "histogram-values";
+const char CameraParameters::KEY_SKIN_TONE_ENHANCEMENT[] = "skinToneEnhancement";
+const char CameraParameters::KEY_SUPPORTED_SKIN_TONE_ENHANCEMENT_MODES[] = "skinToneEnhancement-values";
 const char CameraParameters::KEY_SHARPNESS[] = "sharpness";
 const char CameraParameters::KEY_MAX_SHARPNESS[] = "max-sharpness";
 const char CameraParameters::KEY_CONTRAST[] = "contrast";
@@ -216,6 +218,10 @@ const char CameraParameters::LENSSHADE_DISABLE[] = "disable";
  //Values for Histogram Shading
 const char CameraParameters::HISTOGRAM_ENABLE[] = "enable";
 const char CameraParameters::HISTOGRAM_DISABLE[] = "disable";
+
+//Values for Skin Tone Enhancement Modes
+const char CameraParameters::SKIN_TONE_ENHANCEMENT_ENABLE[] = "enable";
+const char CameraParameters::SKIN_TONE_ENHANCEMENT_DISABLE[] = "disable";
 
 //Values for Continuous AF
 const char CameraParameters::CONTINUOUS_AF_OFF[] = "caf-off";
