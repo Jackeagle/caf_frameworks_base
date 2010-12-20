2015c13fabce89d0f011a28cb9b4b7f65dafc52a
Jaime Lopez <jaimel@codeaurora.org>
2010-12-20 15:04:32 -0800

    frameworks/base: Implement new CDMA RIL messages
    Rename RIL_REQUEST_CDMA_SET_SUBSCRIPTION to RIL_REQUEST_CDMA_SET_SUBSCRIPTION_SOURCE
    Implement:
    - RIL_UNSOL_CDMA_SUBSCRIPTION_SOURCE_CHANGED:
    - RIL_UNSOL_CDMA_PRL_CHANGED
    - RIL_REQUEST_CDMA_GET_SUBSCRIPTION_SOURCE
    - RIL_REQUEST_CDMA_PRL_VERSION
    
    Change-Id: I81e04828fb37eef74a3eff93a716b85c70ba16f8

--- telephony/java/com/android/internal/telephony/test/SimulatedCommands.java
+++ telephony/java/com/android/internal/telephony/test/SimulatedCommands.java
@@ -1473,4 +1473,13 @@ public final class SimulatedCommands extends BaseCommands
     public void getGsmBroadcastConfig(Message response) {
         unimplemented(response);
     }
+
+    public void getCdmaSubscriptionSource(Message response) {
+        unimplemented(response);
+    }
+
+    public void getCdmaPrlVersion(Message response) {
+        unimplemented(response);
+    }
+
 }
