235ff2722d3ecc600dfdeae9299e7cfd4de25ba5
John Joseph <jnjoseph@codeaurora.org>
2010-12-20 15:05:20 -0800

    telephony: ril.java and interface changes to support IPv6 and SVLTE
    ril.java and commands interface changes to reflect following:
    - decouple voice and data registration messages
    - add support for IPV4/IPV6 in setup_data_call
    - LAST_DATA_CALL_FAIL_CAUSE is now obsolete.
    
    Change-Id: If242c3768dcb4eafa0a9a0f77ef46d41076e9caa

--- telephony/java/com/android/internal/telephony/CommandsInterface.java
+++ telephony/java/com/android/internal/telephony/CommandsInterface.java
@@ -255,6 +255,8 @@ public interface CommandsInterface {
     void unregisterForCallStateChanged(Handler h);
     void registerForNetworkStateChanged(Handler h, int what, Object obj);
     void unregisterForNetworkStateChanged(Handler h);
+    void registerForDataNetworkStateChanged(Handler h, int what, Object obj);
+    void unregisterForDataNetworkStateChanged(Handler h);
     void registerForDataStateChanged(Handler h, int what, Object obj);
     void unregisterForDataStateChanged(Handler h);
 
@@ -892,7 +894,7 @@ public interface CommandsInterface {
      * Please note that registration state 4 ("unknown") is treated
      * as "out of service" above
      */
-    void getGPRSRegistrationState (Message response);
+    void getDataRegistrationState (Message response);
 
     /**
      * response.obj.result is a String[3]
@@ -1375,11 +1377,13 @@ public interface CommandsInterface {
      *            the password for APN, or NULL
      * @param authType
      *            the PAP / CHAP auth type. Values is one of SETUP_DATA_AUTH_*
+     * @param ipVersion
+     *            0 - IPV4, 1 - IPV6
      * @param result
      *            Callback message
      */
     public void setupDataCall(String radioTechnology, String profile, String apn,
-            String user, String password, String authType, Message result);
+            String user, String password, String authType, String ipVersion, Message result);
 
     /**
      * Deactivate packet data connection
