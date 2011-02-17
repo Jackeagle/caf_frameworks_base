67f9f6060eaef79eb4f007a3985635afac5ecc04
John Joseph <jnjoseph@codeaurora.org>
2011-02-17 14:08:44 -0800

    Partially revert "telephony/data: dct and all dc should share the same looper"
    - Keep the logging though.
    
    This reverts commit ea5863b3ea9083433df4b0dcfdf70da0c9f13a2f.
    
    Change-Id: I96e37f7698f8c461ed072af499a9f76e28ae0b7f

--- telephony/java/com/android/internal/telephony/DataConnection.java
+++ telephony/java/com/android/internal/telephony/DataConnection.java
@@ -26,7 +26,6 @@ import com.android.internal.util.HierarchicalStateMachine;
 
 import android.content.Context;
 import android.os.AsyncResult;
-import android.os.Looper;
 import android.os.Message;
 import android.os.SystemClock;
 import android.os.SystemProperties;
@@ -207,7 +206,7 @@ public abstract class DataConnection extends HierarchicalStateMachine {
 
    //***** Constructor
     protected DataConnection(Context context, CommandsInterface ci, String name) {
-        super(name, Looper.getMainLooper());
+        super(name);
         if (DBG) log("DataConnection constructor E");
         this.mCM = ci;
         this.mContext = context;
