c21fe61e3d04bf4427b46d672107cf88a494342e
Banavathu, Srinivas Naik <snb@codeaurora.org>
2011-02-28 12:14:00 +0530

    Telephony: STK CC feature implementation.
    Change-Id: I03649955d16b215af56056903aa8960312d279fa
    
    CRs-Fixed: 278601

--- telephony/java/com/android/internal/telephony/BaseCommands.java
+++ telephony/java/com/android/internal/telephony/BaseCommands.java
@@ -91,6 +91,8 @@ public abstract class BaseCommands implements CommandsInterface {
     protected Registrant mEmergencyCallbackModeRegistrant;
     protected Registrant mRingRegistrant;
     protected Registrant mGsmBroadcastSmsRegistrant;
+    protected Registrant mCatCcAlphaRegistrant;
+    protected Registrant mSSRegistrant;
 
     // Network Mode received from PhoneFactory
     protected int mNetworkMode;
@@ -412,6 +414,22 @@ public abstract class BaseCommands implements CommandsInterface {
 
     public void unSetOnCallRing(Handler h) {
         mRingRegistrant.clear();
+    }
+
+    public void setOnSS(Handler h, int what, Object obj) {
+        mSSRegistrant = new Registrant (h, what, obj);
+    }
+
+    public void unSetOnSS(Handler h) {
+        mSSRegistrant.clear();
+    }
+
+    public void setOnCatCcAlphaNotify(Handler h, int what, Object obj) {
+        mCatCcAlphaRegistrant = new Registrant (h, what, obj);
+    }
+
+    public void unSetOnCatCcAlphaNotify(Handler h) {
+        mCatCcAlphaRegistrant.clear();
     }
 
     public void registerForInCallVoicePrivacyOn(Handler h, int what, Object obj) {
