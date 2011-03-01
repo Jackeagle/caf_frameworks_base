f13b8dd427c8d4a7645d5c5c3c4e41ecc575db14
Vairavan Srinivasan <vairav@codeaurora.org>
2011-03-01 11:18:55 -0800

    frameworks/base: Fix to ensure actual delay in a vibrate pattern
    The function delay times out soon when InterruptedException is
    raised during the wait() call, as the value of bedtime is not
    updated in the while loop. Fix is to update the same in the while
    loop.
    
      It might also timeout as value of duration isn't updated properly
    to reflect the elapsed time.
    
    CRs-Fixed: 277271
    
    Change-Id: I1bed3a5bbe291d69e2dd36fb1937457d24c87a1f

--- services/java/com/android/server/VibratorService.java
+++ services/java/com/android/server/VibratorService.java
@@ -296,8 +296,8 @@ public class VibratorService extends IVibratorService.Stub {
 
         private void delay(long duration) {
             if (duration > 0) {
-                long bedtime = SystemClock.uptimeMillis();
                 do {
+                    long bedtime = SystemClock.uptimeMillis();
                     try {
                         this.wait(duration);
                     }
@@ -307,7 +307,7 @@ public class VibratorService extends IVibratorService.Stub {
                         break;
                     }
                     duration = duration
-                            - SystemClock.uptimeMillis() - bedtime;
+                            - (SystemClock.uptimeMillis() - bedtime);
                 } while (duration > 0);
             }
         }
