43feeb38176948f82f134a0c1693135c5d41b51c
Luis GarciaCalderon <luisgc@codeaurora.org>
2011-03-04 18:45:06 -0800

    libstagefright: Enable NT AAC hardware encoder
    Enables the NT AAC hardware encoder.
    
    Change-Id: Ic52cfcd26dcaa8ac30c7b464defe1cd558405505

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -214,6 +214,7 @@ static const CodecInfo kEncoderInfo[] = {
     { MEDIA_MIMETYPE_AUDIO_AMR_NB, "AMRNBEncoder" },
     { MEDIA_MIMETYPE_AUDIO_AMR_WB, "OMX.TI.WBAMR.encode" },
     { MEDIA_MIMETYPE_AUDIO_AMR_WB, "AMRWBEncoder" },
+    { MEDIA_MIMETYPE_AUDIO_AAC, "OMX.qcom.audio.encoder.aac" },
     { MEDIA_MIMETYPE_AUDIO_AAC, "OMX.TI.AAC.encode" },
     { MEDIA_MIMETYPE_AUDIO_AAC, "AACEncoder" },
 //    { MEDIA_MIMETYPE_AUDIO_AAC, "OMX.PV.aacenc" },
@@ -405,6 +406,12 @@ uint32_t OMXCodec::getComponentQuirks(
     }
 
     if (!strcmp(componentName, "OMX.qcom.audio.encoder.qcelp13")) {
+        quirks |= kRequiresAllocateBufferOnInputPorts;
+        quirks |= kRequiresAllocateBufferOnOutputPorts;
+        quirks |= kRequiresEOSMessage;
+    }
+
+    if (!strcmp(componentName, "OMX.qcom.audio.encoder.aac")) {
         quirks |= kRequiresAllocateBufferOnInputPorts;
         quirks |= kRequiresAllocateBufferOnOutputPorts;
         quirks |= kRequiresEOSMessage;
