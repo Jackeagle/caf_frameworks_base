7e766bce30650d2ef26ec4188b1dc479ebf025a7
Stewart Chao <stewartc@codeaurora.org>
2011-03-27 11:44:22 -0400

    libmediaplayerservice/libstagefright: changed yuv format querying
    Updated the method of passing yuv frame format to mediaplayer
    client.  Removed MEDIA_BUFFER_FORMAT event and introduced
    MEDIA_INVOKE_QUERY_BUFFER_FORMAT invoke.  Removed
    MEDIA_BUFFER_EMPTY event.  Event now reported using
    MEDIA_BUFFER_RETURN and the second parameter.
    
    Change-Id: Ib819781b11effd60e4647741bf5a78aef0407a06

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -1319,7 +1319,6 @@ void AwesomePlayer::onVideoEvent() {
                             mColorFormat = colorFormat;
 
                             notifyListener_l(MEDIA_SET_VIDEO_SIZE, mDecodedWidth, mDecodedHeight);
-                            notifyListener_l(MEDIA_BUFFER_FORMAT, mColorFormat);
                             notifyListener_l(MEDIA_FORMAT_CHANGED);
 
                             mFormatChanged = true;
@@ -1493,7 +1492,7 @@ void AwesomePlayer::onVideoEvent() {
         if (bufferAddress != NULL) {
             memcpy(bufferAddress, mVideoBuffer[mVideoQueueBack]->data(), copySize);
 
-            notifyListener_l(MEDIA_BUFFER_READY, frame);
+            notifyListener_l(MEDIA_BUFFER_READY, frame, 1);
         }
     }
 #endif
@@ -1947,17 +1946,6 @@ void AwesomePlayer::finishAsyncPrepare_l() {
             }
         }
 
-#ifdef YUVCLIENT
-        if (mFrameBuffers != NULL && mVideoTrack != NULL) {
-            mDecodedWidth = mVideoWidth;
-            mDecodedHeight = mVideoHeight;
-
-            sp<MetaData> meta = mVideoSource->getFormat();
-            CHECK(meta->findInt32(kKeyColorFormat, &mColorFormat));
-            notifyListener_l(MEDIA_BUFFER_FORMAT, mColorFormat);
-        }
-#endif
-
         notifyListener_l(MEDIA_PREPARED);
     }
 
@@ -2375,6 +2363,19 @@ status_t AwesomePlayer::queueFrameBuffer(int frame) {
     return OK;
 }
 
+status_t AwesomePlayer::queryBufferFormat(int *format) {
+    if (mVideoSource == NULL)
+        mColorFormat = 0;
+    else {
+        sp<MetaData> meta = mVideoSource->getFormat();
+        CHECK(meta->findInt32(kKeyColorFormat, &mColorFormat));
+        mDecodedWidth = mVideoWidth;
+        mDecodedHeight = mVideoHeight;
+    }
+    *format = mColorFormat;
+    return OK;
+}
+
 void* AwesomePlayer::getFrameBufferAddress(int index) {
     if (mFrameBuffers == NULL)
         return NULL;
@@ -2406,7 +2407,7 @@ void AwesomePlayer::sendBackFrameBuffers() {
     while (!mFrameBufferList.isEmpty()) {
         index = mFrameBufferList[0];
         mFrameBufferList.removeAt(0);
-        notifyListener_l(MEDIA_BUFFER_EMPTY, index);
+        notifyListener_l(MEDIA_BUFFER_READY, index, 0);
     }
 }
 
