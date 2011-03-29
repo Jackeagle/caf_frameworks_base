17128d44ab04c5b7cb2911e8b1c9f837aa9b36a5
Amit Pundir <amitpundir@ti.com>
2011-03-29 12:11:43 +0530

    Gingerbread mouse cursor patch
    This mouse cursor patch is provided by Gregoire Gentil from AlwaysInnovating
    http://git.alwaysinnovating.com/cgit.cgi/ai.android/commit/?id=a721f7e2ab6e680257cc9cb5ecaee6238e615ff2
    
    Right click (back operation) and Middle click (menu key) support added by Rowboat.
    
    Signed-off-by: Amit Pundir <amitpundir@ti.com>

--- core/java/android/view/ViewRoot.java
+++ core/java/android/view/ViewRoot.java
@@ -215,6 +215,8 @@ public final class ViewRoot extends Handler implements ViewParent,
 
     final ViewConfiguration mViewConfiguration;
 
+    static IWindowManager wm;
+
     /**
      * see {@link #playSoundEffect(int)}
      */
@@ -227,9 +229,8 @@ public final class ViewRoot extends Handler implements ViewParent,
             if (!mInitialized) {
                 try {
                     InputMethodManager imm = InputMethodManager.getInstance(mainLooper);
-                    sWindowSession = IWindowManager.Stub.asInterface(
-                            ServiceManager.getService("window"))
-                            .openSession(imm.getClient(), imm.getInputContext());
+                    wm = IWindowManager.Stub.asInterface(ServiceManager.getService("window"));
+                    sWindowSession = wm.openSession(imm.getClient(), imm.getInputContext());
                     mInitialized = true;
                 } catch (RemoteException e) {
                 }
@@ -2174,6 +2175,7 @@ public final class ViewRoot extends Handler implements ViewParent,
             mTranslator.translateEventInScreenToAppWindow(event);
         }
         
+	boolean isMouse = ((event.getSource() & InputDevice.SOURCE_MOUSE) ^ InputDevice.SOURCE_MOUSE) == 0;
         boolean handled;
         if (mView != null && mAdded) {
 
@@ -2837,7 +2839,15 @@ public final class ViewRoot extends Handler implements ViewParent,
 
     private void dispatchMotion(MotionEvent event, boolean sendDone) {
         int source = event.getSource();
-        if ((source & InputDevice.SOURCE_CLASS_POINTER) != 0) {
+	if (((source & InputDevice.SOURCE_MOUSE) ^ InputDevice.SOURCE_MOUSE) == 0) {
+	    try{
+                wm.moveMouseSurface((int)event.getX() - (int)event.getXOffset(),
+                                    (int)event.getY() - (int)event.getYOffset());
+               }catch (RemoteException e){
+                       Log.e(TAG,"RemoteException thrown in moveMouseSurface");
+               }
+            dispatchPointer(event, sendDone);
+        }else if ((source & InputDevice.SOURCE_CLASS_POINTER) != 0) {
             dispatchPointer(event, sendDone);
         } else if ((source & InputDevice.SOURCE_CLASS_TRACKBALL) != 0) {
             dispatchTrackball(event, sendDone);
