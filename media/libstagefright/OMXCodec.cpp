8edf3d8a985c70b24af9c6e094a29d8be203f754
Don Darling <ddarling@ti.com>
2011-05-19 17:20:05 -0500

    Update declared OMX version to 1.1.0
    This change resolves a OMX_ErrorVersionMismatch error from having a mismatch
    between the OMX versions in the StageFright data structures and those reported
    by the OpenMax components being used.  Everything is using OpenMax 1.1.2, but
    StageFright is declaring 1.0.0 and the components are declaring 1.1.0.  This
    change makes them align on 1.1.0, but ultimately both should be set to 1.1.2.

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -410,7 +410,7 @@ template<class T>
 static void InitOMXParams(T *params) {
     params->nSize = sizeof(T);
     params->nVersion.s.nVersionMajor = 1;
-    params->nVersion.s.nVersionMinor = 0;
+    params->nVersion.s.nVersionMinor = 1;
     params->nVersion.s.nRevision = 0;
     params->nVersion.s.nStep = 0;
 }
