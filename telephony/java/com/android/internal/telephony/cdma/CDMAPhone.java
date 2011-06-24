ad7341797f73ba1a97e45d74a2620ad0e9a24602
Uma Maheswari Ramalingam <umam@codeaurora.org>
2011-06-24 10:25:51 -0700

    Telephony: Display error message for network search in Cdma
    For network search command in cdma
    the response message is sent back to close the
    loop and display the error message
    
    Change-Id: I0932a1116a1c43feae68679a298de54c06608d7a
    CRs-Fixed: 290268

--- telephony/java/com/android/internal/telephony/cdma/CDMAPhone.java
+++ telephony/java/com/android/internal/telephony/cdma/CDMAPhone.java
@@ -435,6 +435,13 @@ public class CDMAPhone extends PhoneBase {
     public void
     setNetworkSelectionModeAutomatic(Message response) {
         Log.e(LOG_TAG, "method setNetworkSelectionModeAutomatic is NOT supported in CDMA!");
+        if (response != null) {
+            Log.e(LOG_TAG, "setNetworkSelectionModeAutomatic: not possible in CDMA- Posting exception");
+            CommandException ce = new CommandException(
+                    CommandException.Error.REQUEST_NOT_SUPPORTED);
+            AsyncResult.forMessage(response).exception = ce;
+            response.sendToTarget();
+        }
     }
 
     public void unregisterForSuppServiceNotification(Handler h) {
@@ -520,6 +527,11 @@ public class CDMAPhone extends PhoneBase {
     selectNetworkManually(com.android.internal.telephony.gsm.NetworkInfo network,
             Message response) {
         Log.e(LOG_TAG, "selectNetworkManually: not possible in CDMA");
+        if (response != null) {
+            CommandException ce = new CommandException(CommandException.Error.REQUEST_NOT_SUPPORTED);
+            AsyncResult.forMessage(response).exception = ce;
+            response.sendToTarget();
+        }
     }
 
     public void setOnPostDialCharacter(Handler h, int what, Object obj) {
@@ -680,6 +692,11 @@ public class CDMAPhone extends PhoneBase {
 
     public void getAvailableNetworks(Message response) {
         Log.e(LOG_TAG, "getAvailableNetworks: not possible in CDMA");
+        if (response != null) {
+            CommandException ce = new CommandException(CommandException.Error.REQUEST_NOT_SUPPORTED);
+            AsyncResult.forMessage(response).exception = ce;
+            response.sendToTarget();
+        }
     }
 
     public void setOutgoingCallerIdDisplay(int commandInterfaceCLIRMode, Message onComplete) {
