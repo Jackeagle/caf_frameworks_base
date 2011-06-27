7afaaab566bbf384b153a0c722924eae88695aed
Srinu Gorle <sgorle@codeaurora.org>
2011-06-27 19:55:02 +0530

    libstagefright: Added an error check in AudioPlayer to verify the state.
    - In HTTP streaming, If audio video content not interleaved properly,
      pause() and read() get accessed simultaneously from onvideoevent()
      and onBufferingUpdate().
    
    - In the above scenario, When component is in intermediate state(PAUSING),
      OMXCodec::read returning an error UNKNOWN_ERROR and it sets
      AudioPlayer::mReachedEOS. Same error propagates to application layer and
      it ultimately stops the playback.
    
    - Added a check to handle the intermediate state in PAUSE not to return ERROR
      to application to continue on going playback session.
    
    Change-Id: I152baae6c922835fe30cbbac85b3d4f6efaa55da
    CRs-Fixed: 290593

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -4073,6 +4073,9 @@ status_t OMXCodec::read(
 
     Mutex::Autolock autoLock(mLock);
 
+    if (mState == PAUSING) {
+        return INVALID_OPERATION;
+    }
     if (mState != EXECUTING && mState != RECONFIGURING) {
         return UNKNOWN_ERROR;
     }
