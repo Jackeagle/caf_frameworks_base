3eb67c4a79b7203370fd3c47398d11413bf65456
Anshul Jain <anshulj@codeaurora.org>
2011-06-28 14:48:56 -0700

    frameworks/base: Implementation of AT+CDIS
    Following commands are supported
     +CDIS?
     +CDIS=?
     +CDIS=[<text>[,<text>[,...]]]
    
    Change-Id: I05c317da9a153f43794ce5c04eefc81c6578c8b3

--- core/java/com/android/internal/atfwd/AtCmdFwdService.java
+++ core/java/com/android/internal/atfwd/AtCmdFwdService.java
@@ -84,6 +84,13 @@ public class AtCmdFwdService extends IAtCmdFwd.Stub {
         }
 
         try {
+            cmd = new AtCdisCmdHandler(c);
+            mCmdHandlers.put(cmd.getCommandName().toUpperCase(), cmd);
+        } catch (AtCmdHandlerInstantiationException e) {
+            Log.e(LOG_TAG, "Unable to instantiate command", e);
+        }
+
+        try {
             cmd = new AtCssCmdHandler(c);
             mCmdHandlers.put(cmd.getCommandName().toUpperCase(), cmd);
         } catch (AtCmdHandlerInstantiationException e) {
@@ -108,6 +115,7 @@ public class AtCmdFwdService extends IAtCmdFwd.Stub {
             try {
             ret = h.handleCommand(cmd);
             } catch(Throwable e) {
+                Log.e(LOG_TAG, "Exception " + e.toString());
                 ret = new AtCmdResponse(AtCmdResponse.RESULT_ERROR, "+CME ERROR: 2");
             }
         } else {
