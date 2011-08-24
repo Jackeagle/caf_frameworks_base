27165e6d5507e59361c00fd77c418e94be25425a
Siddartha Pothapragada <spotha@codeaurora.org>
2011-08-23 20:15:43 -0700

    Telephony: Repopulating SmsBroadcastConfigInfo list on enable
    SmsBroadcastConfigInfo list is populated with all the possible
    range lists.
    
    (cherry picked from commit 660b0d650358a011b0d1d003752f921e0fa88ed7)
    
    Change-Id: Ieb691b188186f4e5deba492432abadd591ec3efc

--- telephony/java/com/android/internal/telephony/SMSDispatcher.java
+++ telephony/java/com/android/internal/telephony/SMSDispatcher.java
@@ -1110,7 +1110,8 @@ public abstract class SMSDispatcher extends Handler {
             } else if (intent.getAction().equals(Intent.ACTION_DEVICE_STORAGE_NOT_FULL)) {
                 mStorageAvailable = true;
                 mCm.reportSmsMemoryStatus(true, obtainMessage(EVENT_REPORT_MEMORY_STATUS_DONE));
-            } else if (intent.getAction().equals(Intents.CB_SMS_RECEIVED_ACTION)) {
+            } else if (intent.getAction().equals(Intents.SMS_CB_RECEIVED_ACTION) ||
+                intent.getAction().equals(Intents.SMS_EMERGENCY_CB_RECEIVED_ACTION)) {
                 // Ignore this intent. Apps will process it.
             } else {
                 // Assume the intent is one of the SMS receive intents that
