e7db81a9b86e8e95a4b66520cb4be5394d9c6110
Sriranjan Srikantam <cssrika@codeaurora.org>
2011-08-24 20:15:29 -0700

    libstagefright: Fix mediaserver crash during mp3 song switch
    - MP3 song switch from music application playlist results in mediaserver
      crash randomly
    
    - One of the buffers is not released before calling the MediaBufferGroup
      destructor. This results in CHECK condition failure as buffer reference
      count doesn't become zero
    
      MP3Decoder acquires a buffer from the MediaBufferGroup but it is still
      in process of decoding and has not returned back to AudioPlayer.
      Meanwhile a song switch is triggered which results in AudioPlayer reset
      function to be called which calls MP3Decoder stop and tries to delete the
      MediaBufferGroup object where the condition in CHECK macro fails as one
      of the buffer is not released
    
    - Inorder to fix this issue AudioSink/Track needs to be stopped first before
      source stop to  allow the callback function to be released which makes
      sure of source read calls to return and releases all the acquired buffers
    
      But there might be another side effect if session is tried to close during
      paused state. There might be a case AudioSink/Track is waiting on audio
      source read when there are no buffers to be returned and sink can only be
      released after audio source stop to make sure the read call gets unblocked
    
      Hence fix this issue by releasing AudioSink/Track before the audio source
      stop if AudioPlayer is not in paused state and release it after the audio
      source stop if AudioPlayer is in paused state.
    
    CRs-Fixed: 308543
    (cherry picked from commit 706a749140b71729fdaf8aa6a1061eb97c4a1737)
    
    Change-Id: I97703766b7edf7ab82c900e99684a5f13b2ba453

--- media/libstagefright/AudioPlayer.cpp
+++ media/libstagefright/AudioPlayer.cpp
@@ -212,6 +212,27 @@ void AudioPlayer::resume() {
 void AudioPlayer::reset() {
     CHECK(mStarted);
 
+    /* AudioSink/AudioTrack needs to be stopped before source stop to ensure any
+     * ensure any intermediate AudioSink callback is returned and callback is
+     * released. This makes sure that all buffers acquired from MediaBufferGroup
+     * are released before closing source stop which destructs MediaBufferGroup.
+     * But there is a case where AudioSink/Track keeps waiting for read call if
+     * session is closed in paused state and there no buffers available with the
+     * component to return. Closing source stop before will make sure to unblock
+     * the read call. Hence stop Sink/Track after source stop if in paused state
+     */
+    if (mPaused == false) {
+        if (mAudioSink.get() != NULL) {
+            mAudioSink->stop();
+            mAudioSink->close();
+        } else {
+            mAudioTrack->stop();
+
+            delete mAudioTrack;
+            mAudioTrack = NULL;
+        }
+    }
+
     // Make sure to release any buffer we hold onto so that the
     // source is able to stop().
 
@@ -222,7 +243,6 @@ void AudioPlayer::reset() {
 
     if (mInputBuffer != NULL) {
         LOGV("AudioPlayer releasing input buffer.");
-
         mInputBuffer->release();
         mInputBuffer = NULL;
     }
@@ -239,15 +259,22 @@ void AudioPlayer::reset() {
         usleep(1000);
     }
 
-    LOGV("Stop/close mAudioSink and mAudioTrack after mSource is successfully de-inited");
-    if (mAudioSink.get() != NULL) {
-        mAudioSink->stop();
-        mAudioSink->close();
-    } else {
-        mAudioTrack->stop();
+    /* There is a case where AudioSink/Track keeps waiting for read call if
+     * session is closed in paused state and there no buffers available with
+     * the component to return. Closing source stop before will make the read to
+     * unblock. Hence stop AudioSink/Track after source stop if in paused state.
+     */
+    if (mPaused == true) {
+        LOGV("Stop/close mAudioSink and mAudioTrack after mSource is successfully de-inited");
+        if (mAudioSink.get() != NULL) {
+            mAudioSink->stop();
+            mAudioSink->close();
+        } else {
+            mAudioTrack->stop();
 
-        delete mAudioTrack;
-        mAudioTrack = NULL;
+            delete mAudioTrack;
+            mAudioTrack = NULL;
+        }
     }
 
     IPCThreadState::self()->flushCommands();
