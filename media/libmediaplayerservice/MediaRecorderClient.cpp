5ce8aea8e9046b1fae958d644f5e416dcf37feb6
Srinu Gorle <sgorle@codeaurora.org>
2011-09-19 12:57:12 +0530

    libstagefright: Fix for memory leak in Audio/Video recording.
    -MediaRecorder object not getting destructed due to object count grater than one.
    
    -Assign NULL to mListener to make sure MediaRecorder dereferenced.
    
    CRs-Fixed: 304054
    (cherry picked from commit a4ab47ca6183915e83844acded9b2e36fc7976c4)
    
    Change-Id: Icf7916518c6673019dc60638a633283964e4f65f

--- media/libmediaplayerservice/MediaRecorderClient.cpp
+++ media/libmediaplayerservice/MediaRecorderClient.cpp
@@ -338,6 +338,7 @@ status_t MediaRecorderClient::release()
     if (mRecorder != NULL) {
         delete mRecorder;
         mRecorder = NULL;
+        mListener = NULL;
         wp<MediaRecorderClient> client(this);
         mMediaPlayerService->removeMediaRecorderClient(client);
     }
