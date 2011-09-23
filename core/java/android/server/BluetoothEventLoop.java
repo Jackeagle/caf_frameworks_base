3074d832dd3bda06e5041b82339ac2490df0c288
Sunil Joseph <sunilj@codeaurora.org>
2011-09-23 16:39:12 +0530

    Incoming Bluetooth Connection requests - dialog.
    (cherry picked from commit b789250cec8007b6e4ad12250e1bc071714ad989)
    
    Conflicts:
    
    	core/java/android/bluetooth/BluetoothDevice.java
    	core/java/android/server/BluetoothEventLoop.java
    	core/jni/android_server_BluetoothEventLoop.cpp
    
    Change-Id: I2f4c9731cd9a76b510e74fad9fdebdcc171441fe
    (cherry picked from commit 1ad00049aa74ce6912985a371373fe8928fe7d82)
    (cherry picked from commit a77067682c239bb8ec517c798f148b158c0ca176)

--- core/java/android/server/BluetoothEventLoop.java
+++ core/java/android/server/BluetoothEventLoop.java
@@ -49,6 +49,7 @@ class BluetoothEventLoop {
     private boolean mInterrupted;
 
     private final HashMap<String, Integer> mPasskeyAgentRequestData;
+    private final HashMap<String, Integer> mAuthorizationAgentRequestData;
     private final BluetoothService mBluetoothService;
     private final BluetoothAdapter mAdapter;
     private final Context mContext;
@@ -115,6 +116,7 @@ class BluetoothEventLoop {
         mBluetoothService = bluetoothService;
         mContext = context;
         mPasskeyAgentRequestData = new HashMap();
+        mAuthorizationAgentRequestData = new HashMap<String, Integer>();
         mAdapter = adapter;
         initializeNativeDataNative();
     }
@@ -129,6 +131,10 @@ class BluetoothEventLoop {
 
     /* package */ HashMap<String, Integer> getPasskeyAgentRequestData() {
         return mPasskeyAgentRequestData;
+    }
+
+    /* package */ HashMap<String, Integer> getAuthorizationAgentRequestData() {
+        return mAuthorizationAgentRequestData;
     }
 
     /* package */ void start() {
@@ -521,16 +527,19 @@ class BluetoothEventLoop {
         mContext.sendBroadcast(intent, BLUETOOTH_ADMIN_PERM);
     }
 
-    private boolean onAgentAuthorize(String objectPath, String deviceUuid) {
+    private void  onAgentAuthorize(String objectPath, String deviceUuid, int nativeData) {
         String address = mBluetoothService.getAddressFromObjectPath(objectPath);
         if (address == null) {
             Log.e(TAG, "Unable to get device address in onAuthAgentAuthorize");
-            return false;
+            return;
         }
 
         boolean authorized = false;
         ParcelUuid uuid = ParcelUuid.fromString(deviceUuid);
         BluetoothA2dp a2dp = new BluetoothA2dp(mContext);
+
+        BluetoothDevice device = mAdapter.getRemoteDevice(address);
+        mAuthorizationAgentRequestData.put(address, new Integer(nativeData));
 
         // Bluez sends the UUID of the local service being accessed, _not_ the
         // remote service
@@ -538,10 +547,9 @@ class BluetoothEventLoop {
                 (BluetoothUuid.isAudioSource(uuid) || BluetoothUuid.isAvrcpTarget(uuid)
                         || BluetoothUuid.isAdvAudioDist(uuid)) &&
                         !isOtherSinkInNonDisconnectingState(address)) {
-            BluetoothDevice device = mAdapter.getRemoteDevice(address);
             authorized = a2dp.getSinkPriority(device) > BluetoothA2dp.PRIORITY_OFF;
             if (authorized) {
-                Log.i(TAG, "Allowing incoming A2DP / AVRCP connection from " + address);
+                Log.i(TAG, "First check pass for incoming A2DP / AVRCP connection from " + address);
                 // Some headsets try to connect AVCTP before AVDTP - against the recommendation
                 // If AVCTP connection fails, we get stuck in IncomingA2DP state in the state
                 // machine.  We don't handle AVCTP signals currently. We only send
@@ -554,9 +562,10 @@ class BluetoothEventLoop {
                 // established. If the remote device sends AVRCP connection
                 // first DUT will accept the AVRCP connection and initiates
                 // A2DP connection from handset.
+                    a2dp.allowIncomingConnect(device, authorized);
                     Log.d(TAG,"AVRCP target connection");
                     mBluetoothService.notifyConnectA2dp(address);
-                    return true;
+                    return;
                 }
             } else {
                 Log.i(TAG, "Rejecting incoming A2DP / AVRCP connection from " + address);
@@ -565,7 +574,7 @@ class BluetoothEventLoop {
             Log.i(TAG, "Rejecting incoming " + deviceUuid + " connection from " + address);
         }
         log("onAgentAuthorize(" + objectPath + ", " + deviceUuid + ") = " + authorized);
-        return authorized;
+        if (!authorized) a2dp.allowIncomingConnect(device, authorized);
     }
 
     private boolean onAgentOutOfBandDataAvailable(String objectPath) {
