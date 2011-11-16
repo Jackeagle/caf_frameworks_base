e5f313c2d479116ef4181af2d8ddfe7450cdd09d
Uma Maheswari Ramalingam <umam@codeaurora.org>
2011-11-16 10:12:38 -0800

    Telephony: Disable manual network selection in Global modes
    For multimode targets disable manual network selection
    and force automatic mode network selection
    in Global and Global+LTE modes.
    Global Pre-FIT Test Cases: 5.6.4, 5.6.5, 5.6.8
    
    Display error message for network search in Cdma
    
    Change-Id: I49bfd168e12be95b0713dbbdf0feb12a29b88cbd
    CRs-Fixed: 301939, 302354, 290268

--- telephony/java/com/android/internal/telephony/cdma/CDMAPhone.java
+++ telephony/java/com/android/internal/telephony/cdma/CDMAPhone.java
@@ -409,6 +409,13 @@ public class CDMAPhone extends PhoneBase {
     public void
     setNetworkSelectionModeAutomatic(Message response) {
         Log.e(LOG_TAG, "method setNetworkSelectionModeAutomatic is NOT supported in CDMA!");
+        if (response != null) {
+            Log.e(LOG_TAG, "setNetworkSelectionModeAutomatic: not possible in CDMA- Posting exception");
+            CommandException ce = new CommandException(
+                    CommandException.Error.REQUEST_NOT_SUPPORTED);
+            AsyncResult.forMessage(response).exception = ce;
+            response.sendToTarget();
+        }
     }
 
     public void unregisterForSuppServiceNotification(Handler h) {
@@ -504,6 +511,11 @@ public class CDMAPhone extends PhoneBase {
     selectNetworkManually(OperatorInfo network,
             Message response) {
         Log.e(LOG_TAG, "selectNetworkManually: not possible in CDMA");
+        if (response != null) {
+            CommandException ce = new CommandException(CommandException.Error.REQUEST_NOT_SUPPORTED);
+            AsyncResult.forMessage(response).exception = ce;
+            response.sendToTarget();
+        }
     }
 
     public void setOnPostDialCharacter(Handler h, int what, Object obj) {
@@ -697,6 +709,11 @@ public class CDMAPhone extends PhoneBase {
 
     public void getAvailableNetworks(Message response) {
         Log.e(LOG_TAG, "getAvailableNetworks: not possible in CDMA");
+        if (response != null) {
+            CommandException ce = new CommandException(CommandException.Error.REQUEST_NOT_SUPPORTED);
+            AsyncResult.forMessage(response).exception = ce;
+            response.sendToTarget();
+        }
     }
 
     public void setOutgoingCallerIdDisplay(int commandInterfaceCLIRMode, Message onComplete) {
