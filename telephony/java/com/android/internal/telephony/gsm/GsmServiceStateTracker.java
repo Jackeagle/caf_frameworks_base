048017763b20727e4a43b36d7ee81cd62cd08e8e
Jaime Lopez <jaimel@codeaurora.org>
2011-11-30 15:51:48 -0800

    telephony: Ignore events on a destroyed phone
    It is possible to receive responses from RIL after phone has released
    it's references. Ignore events in such cases, since those events no
    longer make sense, and some phone members have been de-initialized.
    
    Make CallTrackers notify UI when ending calls in dispose, since
    it has already unregistered, and won't receive the CALL_STATE_CHANGED
    
    (cherry picked from commit fd7c971b11b0c8463a14699a26d8b56c0ae46937)
    
    Change-Id: Icd4f69cc456c612dbfd81e0eb71d90825168b147
    CRs-Fixed: 228731, 228005, 415801

--- telephony/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
+++ telephony/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
@@ -192,6 +192,12 @@ public class GsmServiceStateTracker extends ServiceStateTracker {
     private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
+            if (!phone.mIsTheCurrentActivePhone) {
+                Log.e(LOG_TAG, "Received Intent " + intent +
+                        " while being destroyed. Ignoring.");
+                return;
+            }
+
             if (intent.getAction().equals(Intent.ACTION_LOCALE_CHANGED)) {
                 // update emergency string whenever locale changed
                 updateSpnDisplay();
