b9f84030c49b2b2f19949a20be30a81b8ad46060
Arun Joseph <arunjoseph@ti.com>
2012-03-01 11:19:01 +0530

    Load global proxy settings for connection type Ethernet
    Currently proxy settings will be applied only if the existing
    connection type is of type Ethernet.
    
    Change-Id: I41a541902af2e331ec1d34b8ce270459fe92196e
    Signed-off-by: Arun Joseph <arunjoseph@ti.com>

--- services/java/com/android/server/ConnectivityService.java
+++ services/java/com/android/server/ConnectivityService.java
@@ -1992,7 +1992,13 @@ private NetworkStateTracker makeWimaxStateTracker() {
                 }
             }
             if (mNetConfigs[netType].isDefault()) {
-                handleApplyDefaultProxy(newLp.getHttpProxy());
+                if(netType == ConnectivityManager.TYPE_ETHERNET) {
+					log("net Type is ConnectivityManager.TYPE_ETHERNET "+ netType + " Setting mGlobalProxy" );
+                    handleApplyDefaultProxy(mGlobalProxy);
+                }
+                else {
+                    handleApplyDefaultProxy(newLp.getHttpProxy());
+                }
             }
         } else {
             if (VDBG) {
@@ -2812,6 +2818,10 @@ private NetworkStateTracker makeWimaxStateTracker() {
 
         if (mGlobalProxy == null) {
             proxyProperties = mDefaultProxy;
+         }
+        if(mActiveDefaultNetwork == ConnectivityManager.TYPE_ETHERNET) {
+					    log("net Type is ConnectivityManager.TYPE_ETHERNET "+ mActiveDefaultNetwork + "Setting mGlobalProxy" );
+                        handleApplyDefaultProxy(mGlobalProxy);
         }
         //sendProxyBroadcast(proxyProperties);
     }
