68dfd75f2e085287aa18cba36a16bed5f85094a7
Arun Joseph <arunjoseph@ti.com>
2012-03-01 11:15:58 +0530

    Ethernet Support to frameworks/base project.
    Added Ethernet Support to frameworks/base based on
    the patches obtained from android-x86.googlegroups.com
    Follow the discussion at
    http://groups.google.com/group/android-x86/browse_thread/
    thread/30fa23d81cddfab1/463884a657116484
    
    Issues remaining
    
    1) DHCP does not work properly for ethernet
    2) Status Bar does not show the Ethernet Connectivity Status
    
    Signed-off-by: Arun Joseph <arunjoseph@ti.com>
    Change-Id: I0c38fd1954d64560130dc6e53f1c14750ff58337
    Signed-off-by: Amuru SaiDeepika <amurus@codeaurora.org>

--- core/java/android/app/DownloadManager.java
+++ core/java/android/app/DownloadManager.java
@@ -344,6 +344,12 @@ public class DownloadManager {
          */
         public static final int NETWORK_WIFI = 1 << 1;
 
+        /**
+         * Bit flag for {@link #setAllowedNetworkTypes} corresponding to
+         * {@link ConnectivityManager#TYPE_ETHERNET}.
+         */
+        public static final int NETWORK_ETHERNET = 1 << 2;
+
         private Uri mUri;
         private Uri mDestinationUri;
         private List<Pair<String, String>> mRequestHeaders = new ArrayList<Pair<String, String>>();
