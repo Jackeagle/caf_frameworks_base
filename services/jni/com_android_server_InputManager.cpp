d3e666590748c2e16b744aec571f92cd198542ae
Arun Joseph <arunjoseph@ti.com>
2012-03-14 15:30:54 +0530

    PM: Enable Touch Wake Up
    Normally touch events do not wake up the device from sleep.
    Modify frameworks/base to add wake flag to touch events
    when the screen is off.
    The flag is added in interceptMotionBeforeQueueing since
    touch events come in the catagory of motion events.
    This approach does not work for omap3evm as touch events
    are not reported to userspace when display goes off.
    Tested on am335xevm.
    
    Signed-off-by: Arun Joseph <arunjoseph@ti.com>

--- services/jni/com_android_server_InputManager.cpp
+++ services/jni/com_android_server_InputManager.cpp
@@ -808,6 +808,7 @@ void NativeInputManager::interceptMotionBeforeQueueing(nsecs_t when, uint32_t& p
                 policyFlags |= POLICY_FLAG_BRIGHT_HERE;
             }
         } else {
+            policyFlags |= POLICY_FLAG_WAKE_DROPPED;
             JNIEnv* env = jniEnv();
             jint wmActions = env->CallIntMethod(mCallbacksObj,
                         gCallbacksClassInfo.interceptMotionBeforeQueueingWhenScreenOff,
