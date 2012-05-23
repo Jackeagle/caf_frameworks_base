dd5d1621a27d086542211cb80a80b2cdc8431d8f
Luis GarciaCalderon <luisgc@codeaurora.org>
2012-05-23 16:11:24 -0700

    libstagefright: Fix crash when parsing corrupt h.264 clips.
    - Fixes an arithmetic overflow when parsing h.264 clips that contain
    corrupt NAL lengths.
    
    Change-Id: Idd08802b9faa47a7790771f9030a3279eb07de32
    CRs-Fixed: 355571, 375153

--- media/libstagefright/MPEG4Extractor.cpp
+++ media/libstagefright/MPEG4Extractor.cpp
@@ -2499,7 +2499,7 @@ status_t MPEG4Source::read(
                 if (!isMalFormed) {
                     nalLength = parseNALSize(&mSrcBuffer[srcOffset]);
                     srcOffset += mNALLengthSize;
-                    isMalFormed = srcOffset + nalLength > size;
+                    isMalFormed = nalLength > size - srcOffset;
                 }
 
                 if (isMalFormed) {
