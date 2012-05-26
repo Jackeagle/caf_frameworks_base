72024037463e15d510aff3a960b4d838d0db280b
Srinivas Krovvidi <skrovvid@codeaurora.org>
2012-05-26 10:45:15 +0530

    Bluetooth: Validate Unicode characters before sending meta data.
     The Unicode character \uFFFF is not a valid input character. The
     UTF-8 validation in dbus fails when such character is sent as
     part of metadata string. This results in dbus assert. The current
     change is to truncate meta data string when an invalid unicode charcter
     is read.
    
    CRs-fixed: 364512
    (cherry picked from commit f85b5019de5e398666299537bed6d880a7f7e246)
    
    Conflicts:
    
    	core/java/android/server/BluetoothA2dpService.java
    
    Change-Id: I35ad8e28114f857ef22a464109e951b1023b1d93

--- core/java/android/server/BluetoothA2dpService.java
+++ core/java/android/server/BluetoothA2dpService.java
@@ -133,6 +133,35 @@ public class BluetoothA2dpService extends IBluetoothA2dp.Stub {
         }
     };
 
+    private String getValidUtf8String ( String str)
+    {
+        int Char, i;
+        String temp;
+
+        if (str == null) {
+            return null;
+        }
+
+        for (i = 0; i < str.length(); i++) {
+            Char = str.codePointAt(i);
+            if ((Char > 0x10FFFF) ||
+                ((Char & 0xFFFFF800) == 0xD800) ||
+                ((Char >= 0xFDD0) && (Char <= 0xFDEF)) ||
+                ((Char & 0xFFFE) == 0xFFFE)) {
+                break;
+            }
+        }
+
+        if (i != str.length()) {
+            try {
+                temp = new String(str.getBytes(), 0, i, "UTF-8");
+                str = temp;
+            } catch (Exception e) {
+                Log.e(TAG, "Exception" + e);
+            }
+        }
+        return str;
+    }
 
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
@@ -314,15 +343,22 @@ public class BluetoothA2dpService extends IBluetoothA2dp.Stub {
 
                 mUri = uri;
                 try {
+                    String temp;
                     Cursor mCursor = mContext.getContentResolver().query(mUri, mCursorCols,
                                         null, null, null);
                     mCursor.moveToFirst();
-                    mTrackName = mCursor.getString(
+                    temp = mCursor.getString(
                         mCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE));
-                    mArtistName = mCursor.getString(
+
+                    mTrackName = getValidUtf8String(temp);
+                    temp = mCursor.getString(
                         mCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST));
-                    mAlbumName = mCursor.getString(
+
+                    mArtistName = getValidUtf8String(temp);
+                    temp = mCursor.getString(
                         mCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM));
+                    mAlbumName = getValidUtf8String(temp);
+
                     long mediaNumber = mCursor.getLong(
                         mCursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID));
                     mMediaNumber = String.valueOf(mediaNumber);
@@ -335,7 +371,6 @@ public class BluetoothA2dpService extends IBluetoothA2dp.Stub {
                     log("tmpId is " + tmpId);
                     mMediaNumber = String.valueOf(tmpId);
                     log("ID is " + mMediaNumber);
-
                     mCursor = mContext.getContentResolver().query(
                                             MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
                                             new String [] { MediaStore.Audio.Media._ID},
