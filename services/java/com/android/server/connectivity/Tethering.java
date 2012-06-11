d3ba8b51e18a4319b6abe2621c1ce2d90dcec250
Niranjan Pendharkar <npendhar@codeaurora.org>
2012-06-11 16:21:56 -0700

    Tethering: Use HIPRI APN when dun_required property is not set
    Regardless of MOBILE_TYPE_DUN APN being available, if dun_required flag is
    not set, fall back to HIPRI as a preferred APN for the embedded data call.
    
    CRs-fixed: 369794
    (cherry picked from commit 7bdefad085199eafa0ab5dc098fe95fcfaa15709)
    
    Change-Id: Iaf9a47e8c86bc0d3d1373f44d368d594f3759c5b

--- services/java/com/android/server/connectivity/Tethering.java
+++ services/java/com/android/server/connectivity/Tethering.java
@@ -674,10 +674,14 @@ public class Tethering extends INetworkManagementEventObserver.Stub {
                         mUpstreamIfaceTypes.add(HIPRI_TYPE);
                     }
                 }
-            }
-            if (mUpstreamIfaceTypes.contains(DUN_TYPE)) {
-                mPreferredUpstreamMobileApn = ConnectivityManager.TYPE_MOBILE_DUN;
+                /* if DUN is still available, make that a priority */
+                if (mUpstreamIfaceTypes.contains(DUN_TYPE)) {
+                    mPreferredUpstreamMobileApn = ConnectivityManager.TYPE_MOBILE_DUN;
+                } else {
+                    mPreferredUpstreamMobileApn = ConnectivityManager.TYPE_MOBILE_HIPRI;
+                }
             } else {
+                /* dun_required is not set, fall back to HIPRI in that case */
                 mPreferredUpstreamMobileApn = ConnectivityManager.TYPE_MOBILE_HIPRI;
             }
         }
