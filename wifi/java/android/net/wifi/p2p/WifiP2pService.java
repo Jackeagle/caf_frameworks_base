af1693e31260c7f71c908834c577fe92e852009e
Sunil Dutt Undekari <duttus@codeaurora.org>
2012-06-25 12:47:28 +0530

    frameworks/base:Avoiding multiple pop-ups
    Each PD / GO Negotiation Request  indication shall result
    in a pop up pertaining to PBC / pin  method in the GUI,
    thus misleading the user.This is avoided  by having the
    state transitions , so that the very first  event shall
    result in a pop up and all the others in the  current
    context are neglected.
    
    CRs-fixed: 374470
    (cherry picked from commit 69b20974bc4c2931bb2647ea3cb1d22ef5e05196)
    
    Change-Id: I148a735d2aaa0f55642655145809272b645c2c0b

--- wifi/java/android/net/wifi/p2p/WifiP2pService.java
+++ wifi/java/android/net/wifi/p2p/WifiP2pService.java
@@ -116,6 +116,11 @@ public class WifiP2pService extends IWifiP2pManager.Stub {
     private static final int P2P_RESTART_TRIES = 5;
 
     private int mP2pRestartCount = 0;
+    /*
+     *@hide
+     */
+    private int mP2pPdNotify = 0;
+
 
     private String mInvitedDeviceAddress;
 
@@ -945,6 +950,8 @@ public class WifiP2pService extends IWifiP2pManager.Stub {
             switch (message.what) {
                 case WifiMonitor.P2P_GO_NEGOTIATION_REQUEST_EVENT:
                 case WifiMonitor.P2P_INVITATION_RECEIVED_EVENT:
+                case WifiMonitor.P2P_PROV_DISC_SHOW_PIN_EVENT:
+                case WifiMonitor.P2P_PROV_DISC_ENTER_PIN_EVENT:
                     //Ignore additional connection requests
                     break;
                 case GROUP_NEGOTIATION_USER_ACCEPT:
@@ -1194,13 +1201,25 @@ public class WifiP2pService extends IWifiP2pManager.Stub {
                      sendP2pPeersChangedBroadcast();
                     break;
                 case WifiMonitor.P2P_PROV_DISC_PBC_REQ_EVENT:
-                    notifyP2pProvDiscPbcRequest((WifiP2pDevice) message.obj);
+                    if (mP2pPdNotify == 0)
+                    {
+                        mP2pPdNotify =1;
+                        notifyP2pProvDiscPbcRequest((WifiP2pDevice) message.obj);
+                    }
                     break;
                 case WifiMonitor.P2P_PROV_DISC_SHOW_PIN_EVENT:
-                    notifyP2pProvDiscShowPinRequest((WifiP2pDevice) message.obj);
+                    if (mP2pPdNotify == 0)
+                    {
+                        mP2pPdNotify =1;
+                        notifyP2pProvDiscShowPinRequest((WifiP2pDevice) message.obj);
+                    }
                     break;
                 case WifiMonitor.P2P_PROV_DISC_ENTER_PIN_EVENT:
-                    notifyP2pProvDiscEnterPinRequest((WifiP2pDevice) message.obj);
+                    if (mP2pPdNotify == 0)
+                    {
+                        mP2pPdNotify =1;
+                        notifyP2pProvDiscEnterPinRequest((WifiP2pDevice) message.obj);
+                    }
                     break;
                 case WifiMonitor.P2P_GROUP_STARTED_EVENT:
                     Slog.e(TAG, "Duplicate group creation event notice, ignore");
@@ -1404,9 +1423,17 @@ public class WifiP2pService extends IWifiP2pManager.Stub {
                         public void onClick(DialogInterface dialog, int which) {
                                 if (DBG) logd(getName() + " wps_pbc");
                                 sendMessage(WPS_PBC);
+                                if (mP2pPdNotify == 1)
+                                    mP2pPdNotify =0;
                         }
                     })
-            .setNegativeButton(r.getString(R.string.cancel), null)
+            .setNegativeButton(r.getString(R.string.cancel), new OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                        if (mP2pPdNotify ==1)
+                            mP2pPdNotify =0;
+                        }
+                    })
             .setCancelable(false)
             .create();
 
@@ -1436,6 +1463,8 @@ public class WifiP2pService extends IWifiP2pManager.Stub {
                     if (mWifiP2pInfo.groupFormed){
                         if (DBG) logd(getName() + " wps_pin");
                         sendMessage(WPS_PIN, tempPin);
+                        if (mP2pPdNotify ==1)
+                            mP2pPdNotify =0;
                     } else {
                             WifiNative.p2pConnectDisplay(tempDevAddress, tempPin, join);
                          }
@@ -1444,7 +1473,13 @@ public class WifiP2pService extends IWifiP2pManager.Stub {
             .setNegativeButton(r.getString(R.string.cancel), new OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
+                        if (mWifiP2pInfo.groupFormed){
+                            if (mP2pPdNotify ==1)
+                                mP2pPdNotify =0;
+                        }
+                        else {
                             transitionTo(mInactiveState);
+                        }
                         }
                     })
             .setCancelable(false)
@@ -1473,6 +1508,8 @@ public class WifiP2pService extends IWifiP2pManager.Stub {
                         if (DBG) logd(getName() + " wps_pin");
                         if (mWifiP2pInfo.groupFormed){
                             sendMessage(WPS_PIN, pin.getText().toString());
+                            if (mP2pPdNotify ==1)
+                                mP2pPdNotify =0;
                         }
                         else {
                             WifiNative.p2pConnectKeypad(tempDeviceAddress, pin.getText().toString(), join);
@@ -1482,7 +1519,13 @@ public class WifiP2pService extends IWifiP2pManager.Stub {
              .setNegativeButton(r.getString(R.string.cancel), new OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                        transitionTo(mInactiveState);
+                        if (mWifiP2pInfo.groupFormed){
+                            if (mP2pPdNotify ==1)
+                                mP2pPdNotify =0;
+                        }
+                        else {
+                            transitionTo(mInactiveState);
+                        }
                         }
                     })
 
