6716ad9c64467e01b268108909c2ef64430b6b9f
Jiju Kinattingal <jijuk@codeaurora.org>
2012-07-09 21:19:52 +0530

    Telephony: Reset the data registration state on radio off/unavailable
    Change-Id: Ib63105fbe44fc76ad81d47a3928a002c84d7c3e1
    CRs-Fixed: 380373

--- telephony/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
+++ telephony/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
@@ -855,6 +855,7 @@ class GsmServiceStateTracker extends ServiceStateTracker {
         switch (cm.getRadioState()) {
             case RADIO_UNAVAILABLE:
                 newSS.setStateOutOfService();
+                newGPRSState = ServiceState.STATE_OUT_OF_SERVICE;
                 newCellLoc.setStateInvalid();
                 setSignalStrengthDefaultValues();
                 mGotCountryCode = false;
@@ -863,6 +864,7 @@ class GsmServiceStateTracker extends ServiceStateTracker {
 
             case RADIO_OFF:
                 newSS.setStateOff();
+                newGPRSState = ServiceState.STATE_POWER_OFF;
                 newCellLoc.setStateInvalid();
                 setSignalStrengthDefaultValues();
                 mGotCountryCode = false;
