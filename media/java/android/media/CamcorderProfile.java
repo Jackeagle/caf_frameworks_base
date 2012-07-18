2e6aff7b494580a913821872b79ed88a6bd46fb4
Guruprasad Gaonkar <ggaonk@codeaurora.org>
2012-07-18 16:19:14 -0700

    Camera: Framework changes to support camcorder profiles
    Change-Id: If90bd28bd0fd93f29a443562ac4875da9d5f4f8d

--- media/java/android/media/CamcorderProfile.java
+++ media/java/android/media/CamcorderProfile.java
@@ -90,9 +90,30 @@ public class CamcorderProfile
      */
     public static final int QUALITY_QVGA = 7;
 
+    /**
+     * {@hide}
+     */
+    public static final int QUALITY_WQVGA = 11;
+
+    /**
+     * {@hide}
+     */
+    public static final int QUALITY_VGA = 10;
+
+    /**
+     * {@hide}
+     */
+    public static final int QUALITY_WVGA = 9;
+
+    /**
+     * {@hide}
+     */
+    public static final int QUALITY_FWVGA = 8;
+
+
     // Start and end of quality list
     private static final int QUALITY_LIST_START = QUALITY_LOW;
-    private static final int QUALITY_LIST_END = QUALITY_QVGA;
+    private static final int QUALITY_LIST_END = QUALITY_WQVGA;
 
     /**
      * Time lapse quality level corresponding to the lowest available resolution.
