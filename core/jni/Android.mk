142e3d8d732bc098a7969fe088208b65d77d983d
David Ng <dave@codeaurora.org>
2012-07-27 17:17:14 -0700

    ActivityTrigger:  New class to invoke when activity starts/resumes
    Change-Id: Ib57aa4d3767e27b5bba658b27e084fc3ef042c36

--- core/jni/Android.mk
+++ core/jni/Android.mk
@@ -150,6 +150,9 @@ LOCAL_SRC_FILES:= \
 	android_content_res_ObbScanner.cpp \
 	android_content_res_Configuration.cpp \
     android_animation_PropertyValuesHolder.cpp
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+LOCAL_SRC_FILES += com_android_internal_app_ActivityTrigger.cpp
+endif
 
 LOCAL_C_INCLUDES += \
 	$(JNI_H_INCLUDE) \
