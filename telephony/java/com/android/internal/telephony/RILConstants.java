2f518a3b7d04f9b2134986b628783afa763c3eae
Rika Brooks <rbrooks@codeaurora.org>
2012-08-07 14:51:19 -0700

    Telephony: SMS over IMS
    Add RIL_REQUEST_IMS_REGISTRATION_STATE, RIL_REQUEST_IMS_SEND_SMS, and
    RIL_UNSOL_RESPONSE_IMS_NETWORK_STATE_CHANGED.  Add FORMAT_UNKNOWN to
    SmsConstants.
    
    Change-Id: Ie43608a643019aa7895a2d78a2ef7d88868e8f67

--- telephony/java/com/android/internal/telephony/RILConstants.java
+++ telephony/java/com/android/internal/telephony/RILConstants.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2006 The Android Open Source Project
+ * Copyright (c) 2012, The Linux Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -260,6 +261,8 @@ cat include/telephony/ril.h | \
     int RIL_REQUEST_ACKNOWLEDGE_INCOMING_GSM_SMS_WITH_PDU = 106;
     int RIL_REQUEST_STK_SEND_ENVELOPE_WITH_STATUS = 107;
     int RIL_REQUEST_VOICE_RADIO_TECH = 108;
+    int RIL_REQUEST_IMS_REGISTRATION_STATE = 109;
+    int RIL_REQUEST_IMS_SEND_SMS = 110;
     int RIL_UNSOL_RESPONSE_BASE = 1000;
     int RIL_UNSOL_RESPONSE_RADIO_STATE_CHANGED = 1000;
     int RIL_UNSOL_RESPONSE_CALL_STATE_CHANGED = 1001;
@@ -297,4 +300,5 @@ cat include/telephony/ril.h | \
     int RIL_UNSOL_EXIT_EMERGENCY_CALLBACK_MODE = 1033;
     int RIL_UNSOL_RIL_CONNECTED = 1034;
     int RIL_UNSOL_VOICE_RADIO_TECH_CHANGED = 1035;
+    int RIL_UNSOL_RESPONSE_IMS_NETWORK_STATE_CHANGED = 1036;
 }
