1e34ddb259c40b27aec2a10c7279c1b294871f57
Gubbala Venugopal Rao <vgubba@codeaurora.org>
2012-08-23 12:27:16 +0530

    Bluetooth: Resolved the hang in settings menu when accepting pairing            request from HFP1.6 headset.
    Found that there is a deadlock occured when BluetoothService object and
    BluetoothHeadsetService object are locked in Hold and wait condition.
    
    Change-Id: I48cd150baf8a1f344292924b50b017ab7514c069
    CRs-Fixed: 388809

--- core/java/android/server/BluetoothService.java
+++ core/java/android/server/BluetoothService.java
@@ -220,6 +220,7 @@ public class BluetoothService extends IBluetooth.Stub {
     private static String mDockAddress;
     private String mDockPin;
 
+    private Object mAllowConnectLock = new Object();
     private boolean mAllowConnect = true;
 
     private int mAdapterConnectionState = BluetoothAdapter.STATE_DISCONNECTED;
@@ -3596,7 +3597,7 @@ public class BluetoothService extends IBluetooth.Stub {
     }
 
     private void autoConnect() {
-        synchronized (this) {
+        synchronized (mAllowConnectLock) {
             if (!mAllowConnect) {
                 Log.d(TAG, "Not auto-connecting devices because of temporary BT on state.");
                 return;
@@ -3639,7 +3640,7 @@ public class BluetoothService extends IBluetooth.Stub {
     }
 
     public boolean notifyIncomingConnection(String address, boolean rejected) {
-        synchronized (this) {
+        synchronized (mAllowConnectLock) {
             if (!mAllowConnect) {
                 Log.d(TAG, "Not allowing incoming connection because of temporary BT on state.");
                 return false;
@@ -3667,7 +3668,7 @@ public class BluetoothService extends IBluetooth.Stub {
     }
 
     /*package*/ boolean notifyIncomingA2dpConnection(String address, boolean rejected) {
-        synchronized (this) {
+        synchronized (mAllowConnectLock) {
             if (!mAllowConnect) {
                 Log.d(TAG, "Not allowing a2dp connection because of temporary BT on state.");
                 return false;
