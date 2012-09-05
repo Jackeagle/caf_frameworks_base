3abda403f80e931893495f7e90144a550aa4825f
Selim Gurun <sgurun@google.com>
2012-09-05 11:53:44 -0700

    DO NOT MERGE remove defunct javascript interface
    Bug: 7073422
    
    The feature that is using search box implementation was removed from
    browser after Honeycomb. This is to remove the js interface that
    is added for this feature.
    
    Clean cherry pick of d773ca8ff2a7a5be94d7f2aaa8ff5ef5dac501a8
    
    Change-Id: I5cad7142ad75d402d32d879a11f4cc8807e9ba29
    (cherry picked from commit b56046995a7eecf95df346c20a4f41fa10766013)

--- core/java/android/webkit/BrowserFrame.java
+++ core/java/android/webkit/BrowserFrame.java
@@ -249,7 +249,6 @@ class BrowserFrame extends Handler {
         mWebViewCore = w;
 
         mSearchBox = new SearchBoxImpl(mWebViewCore, mCallbackProxy);
-        mJavaScriptObjects.put(SearchBoxImpl.JS_INTERFACE_NAME, mSearchBox);
 
         AssetManager am = context.getAssets();
         nativeCreateFrame(w, am, proxy.getBackForwardList());
@@ -602,8 +601,6 @@ class BrowserFrame extends Handler {
             }
         }
         mRemovedJavaScriptObjects.clear();
-
-        stringByEvaluatingJavaScriptFromString(SearchBoxImpl.JS_BRIDGE);
     }
 
     /**
