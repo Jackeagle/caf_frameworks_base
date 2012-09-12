ec36abdb21c9ca6abd5129dd5d23887cb4ab27a1
Archana Ramachandran <archanar@codeaurora.org>
2012-09-11 18:10:14 -0700

    Bluetooth: Fix closing gatt services not disconnecting LE connection
    The reference count for the Gatt service is decremented only
    when close service is initiated by the gatt client profile and
    not on disconnection from the remote device. Added fix to clear the
    ref count for all the Gatt services present in the remote
    on receiving ACL DISCONNECTED intent.
    
    CRs-fixed: 398560
    (cherry picked from commit 106d536a753c6234228ae07922112dc00b28600a)
    
    Change-Id: I23c107a326dccd884f4f89e0c4d78be4bc150bbf

--- core/java/android/server/BluetoothEventLoop.java
+++ core/java/android/server/BluetoothEventLoop.java
@@ -557,6 +557,8 @@ class BluetoothEventLoop {
                           BluetoothDevice.BOND_NONE);
                 }
 
+                mBluetoothService.clearGattServicesRefCount(address);
+
                 if (mBluetoothService.getBondState(address) == BluetoothDevice.BOND_NONE)
                     mBluetoothService.clearRemoteDeviceGattServices(address);
 
