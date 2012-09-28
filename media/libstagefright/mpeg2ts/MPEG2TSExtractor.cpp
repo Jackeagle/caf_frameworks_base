Jackeagle/caf_frameworks_av

mpeg2ts: adjust clip size to multiple of packet size
ts clips having size non-multiple of tspacketsize were
unplayable. In ts extractor, we check if the size is
non-multiple of packet size, it return without creating
any source.This change modify the clipsize to nearest
multiple of packetsize.

Change-Id: Icdebd227615b38c2b543ac90cbb756dc03315a05
CRs-Fixed: 382682

media/libstagefright/mpeg2ts/MPEG2TSExtractor.cpp
@@ -446,18 +446,18 @@ MPEG2TSExtractor::MPEG2TSExtractor(const sp<DataSource> &source)
     CHECK(kTSPacketSize != 0);
 
     mDataSource->getSize(&mClipSize);
+
+    if (mClipSize % kTSPacketSize != 0) {
+        mClipSize = mClipSize - (mClipSize % kTSPacketSize);
+        ALOGI("Clipsize %lld is adjusted to multiple of TSPacketSize %lld", mClipSize,kTSPacketSize);
+    }
+
     if (mClipSize == 0) {
         mTSBuffer = new TSBuffer(kTSCacheSize, kTSCacheSize);
     } else {
         mTSBuffer = new TSBuffer(kTSCacheSize, mClipSize);
     }
     CHECK(mTSBuffer != NULL);
-
-    if (mClipSize % kTSPacketSize != 0) {
-        ALOGW("Clipsize is not correct %lld", mClipSize);
-        mClipSize = 0;
-        return;
-    }
 
     init();
 }

Jackeagle/caf_frameworks_base

(cherry picked from commit 9029c2628e1a8f48b06e805b51200af40441c607)
Conflicts:
	media/libstagefright/mpeg2ts/MPEG2TSExtractor.cpp
Change-Id: Ibd861a7bf6947bb670ec342485f3b4b1d036eb7d

media/libstagefright/mpeg2ts/MPEG2TSExtractor.cpp
@@ -533,18 +533,18 @@ MPEG2TSExtractor::MPEG2TSExtractor(const sp<DataSource> &source)
     CHECK(kTSPacketSize != 0);
 
     mDataSource->getSize(&mClipSize);
+
+    if (mClipSize % kTSPacketSize != 0) {
+        mClipSize = mClipSize - (mClipSize % kTSPacketSize);
+        LOGI("Clipsize %lld is adjusted to multiple of TSPacketSize %lld", mClipSize,kTSPacketSize);
+    }
+
     if (mClipSize == 0) {
         mTSBuffer = new TSBuffer(kTSCacheSize, kTSCacheSize);
     } else {
         mTSBuffer = new TSBuffer(kTSCacheSize, mClipSize);
     }
     CHECK(mTSBuffer != NULL);
-
-    if (mClipSize % kTSPacketSize != 0) {
-        LOGW("Clipsize is not correct %lld", mClipSize);
-        mClipSize = 0;
-        return;
-    }
 
     init();
 }