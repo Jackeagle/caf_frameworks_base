Jackeagle/caf_frameworks_av

mpeg2ts: fix compilation warning
Change-Id: Ibfd74cccb2436318b9d505c4287fad504d28a119
CRs-Fixed: 382682

media/libstagefright/mpeg2ts/MPEG2TSExtractor.cpp
@@ -449,7 +449,7 @@ MPEG2TSExtractor::MPEG2TSExtractor(const sp<DataSource> &source)
 
     if (mClipSize % kTSPacketSize != 0) {
         mClipSize = mClipSize - (mClipSize % kTSPacketSize);
-        ALOGI("Clipsize %lld is adjusted to multiple of TSPacketSize %lld", mClipSize,kTSPacketSize);
+        ALOGI("Clipsize %lld is adjusted to multiple of TSPacketSize %d", mClipSize,kTSPacketSize);
     }
 
     if (mClipSize == 0) {

Jackeagle/caf_frameworks_base

(cherry picked from commit 56fa0d773ea1012d33b93e6c7af8ec1ffdacc9d0)
Conflicts:
	media/libstagefright/mpeg2ts/MPEG2TSExtractor.cpp
Change-Id: I108c6eaf65c124c28d67dc4a8602bdd5a00f793d

media/libstagefright/mpeg2ts/MPEG2TSExtractor.cpp
@@ -536,7 +536,7 @@ MPEG2TSExtractor::MPEG2TSExtractor(const sp<DataSource> &source)
 
     if (mClipSize % kTSPacketSize != 0) {
         mClipSize = mClipSize - (mClipSize % kTSPacketSize);
-        LOGI("Clipsize %lld is adjusted to multiple of TSPacketSize %lld", mClipSize,kTSPacketSize);
+        LOGI("Clipsize %lld is adjusted to multiple of TSPacketSize %d", mClipSize,kTSPacketSize);
     }
 
     if (mClipSize == 0) {