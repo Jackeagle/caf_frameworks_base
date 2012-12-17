11e3f881375891fdedc18107f2035a9a3be0829c
Anirudh Ghayal <aghayal@codeaurora.org>
2012-12-17 11:31:22 +0530

    SystemUI: BatteryController: Update the battery icon based on charging state
    The charging indication on the battery icon is needed only when the battery
    is charging. Update this icon based on the battery charging status.
    
    Change-Id: If5dbd27aad7852277d0db3ca1a034cfcd6b22850
    Signed-off-by: Anirudh Ghayal <aghayal@codeaurora.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryController.java
@@ -57,8 +57,10 @@ public class BatteryController extends BroadcastReceiver {
         if (action.equals(Intent.ACTION_BATTERY_CHANGED)) {
             final int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
             final boolean plugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0) != 0;
-            final int icon = plugged ? R.drawable.stat_sys_battery_charge 
-                                     : R.drawable.stat_sys_battery;
+            final int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS,
+						BatteryManager.BATTERY_STATUS_UNKNOWN);
+            final int icon = (BatteryManager.BATTERY_STATUS_CHARGING == status)
+                    ? R.drawable.stat_sys_battery_charge : R.drawable.stat_sys_battery;
             int N = mIconViews.size();
             for (int i=0; i<N; i++) {
                 ImageView v = mIconViews.get(i);
