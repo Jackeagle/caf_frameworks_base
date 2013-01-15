b76539040ce0357db367b1d42721b2f680318dac
Duy Truong <dtruong@codeaurora.org>
2013-01-15 11:16:17 -0800

    Update copyright to The Linux Foundation
    Change-Id: Ib2e0db6c0f95ddd2c7f4df7ad3f93dcf07ef4dfb

--- telephony/java/com/android/internal/telephony/PhoneProxy.java
+++ telephony/java/com/android/internal/telephony/PhoneProxy.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2008 The Android Open Source Project
- * Copyright (c) 2011-2012, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2011-2012, The Linux Foundation. All rights reserved.
  *
  * Not a Contribution, Apache license notifications and license are retained
  * for attribution purposes only
