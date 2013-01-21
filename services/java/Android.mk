cf0f963ddc988cd47e0b5adfde6e4dd6b3f2861c
Amara Venkata Mastan Manoj Kumar <manojavm@codeaurora.org>
2013-01-21 13:45:23 -0800

    frameworks/base: Use QCRemoteDisplay to launch Wifi Display
    Change-Id: I67ae0c0d2195e72ef5a76856a7c1ca5b7b6c61d9
    (cherry picked from commit 34eefeee7f52ff0e14a209b90a2183edd43a2b70)

--- services/java/Android.mk
+++ services/java/Android.mk
@@ -1,4 +1,5 @@
 LOCAL_PATH:= $(call my-dir)
+WFD_DISABLE_PLATFORM_LIST := msm8610 msm8226
 
 # the library
 # ============================================================
@@ -16,6 +17,14 @@ ifeq ($(BOARD_HAVE_BLUETOOTH_BLUEZ), true)
 endif
 
 LOCAL_MODULE:= services
+
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+ifneq ($(call is-board-platform-in-list,$(WFD_DISABLE_PLATFORM_LIST)),true)
+
+LOCAL_STATIC_JAVA_LIBRARIES := WfdCommon
+
+endif #DISABLE PLATFORMS
+endif #TARGET_USES_WFD
 
 LOCAL_JAVA_LIBRARIES := android.policy telephony-common
 
