595d3d59f4b5de654263b92ff10c81eea104ba7d
Sandeep Gutta <sangutta@codeaurora.org>
2013-02-06 20:25:34 +0530

    Telephony: Enable systemui logs
    Add logs for SystemUi. This would help in
    debugging issues.
    
    Change-Id: If98f7aa4f88a146098c807e3ac117a08f7334e62

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardViewMediator.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardViewMediator.java
@@ -376,7 +376,7 @@ public class KeyguardViewMediator {
 
         @Override
         public void onSimStateChanged(IccCardConstants.State simState) {
-            if (DEBUG) Log.d(TAG, "onSimStateChanged: " + simState);
+            Log.d(TAG, "onSimStateChanged: " + simState);
 
             switch (simState) {
                 case NOT_READY:
