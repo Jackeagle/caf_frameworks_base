06d099e7c8923f0bba43bdffa41e15811235bdd4
Krishnankutty Kolathappilly <kkolat@codeaurora.org>
2013-02-22 19:30:43 -0800

    frameworks/base: Add dock_media_audio_enabled to default.xml
    -Audio is not pausing after removal of usb headset. The USB
     headset device is removed from the noisy intent devices if
     dock_media_audio_enabled property is not set.
    -This is not part of the global database. The property would
     be enabled and added to the global database if we launch
     the Setting->Sound.
    -Adding the property to default.xml so it will be part of global
     database and usb device is part of noisy intent.
    CRs-Fixed: 454799
    
    Change-Id: I63b84e5d6dfe36db998edc2467f03d49ee5976de

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2225,6 +2225,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                     R.string.def_car_undock_sound);
             loadStringSetting(stmt, Settings.Global.WIRELESS_CHARGING_STARTED_SOUND,
                     R.string.def_wireless_charging_started_sound);
+            loadIntegerSetting(stmt, Settings.Global.DOCK_AUDIO_MEDIA_ENABLED,
+                    R.integer.def_dock_audio_media_enabled);
 
             loadIntegerSetting(stmt, Settings.Global.DOCK_AUDIO_MEDIA_ENABLED,
                     R.integer.def_dock_audio_media_enabled);
