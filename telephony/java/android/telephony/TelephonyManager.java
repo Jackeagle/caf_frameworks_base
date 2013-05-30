f8b2af98b4b123c76d4298bbb261bbed77f4f6cf
Chaitanya Saggurthi <csaggurt@codeaurora.org>
2013-05-30 21:32:55 +0530

    Frameworks: Add TD-SCDMA support
    1)TD-SCDMA Signal Strength display changes.
    2)Add TD-SCDMA network type support in MobileDataStateTracker class
    to identify TD-SCDMA as UMTS variant and use the relevant
    kernel data stack tcp buffer sizes defined.
    
    + Telephony: Display 3g icons for tdscdma
    
    Updated: As per technology order & keeping in view of
    future changes, add TDSCDMA case along with UMTS type.
    
    Change-Id: I99d5a630616497cff63c49fef7ad9ec6854f325c
    CRs-Fixed: 403258

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -590,6 +590,9 @@ public class TelephonyManager {
     public static final int NETWORK_TYPE_EHRPD = 14;
     /** Current network is HSPA+ */
     public static final int NETWORK_TYPE_HSPAP = 15;
+    /** Current network is TD_SCDMA {@hide} */
+    public static final int NETWORK_TYPE_TD_SCDMA = 17;
+
 
     /**
      * @return the NETWORK_TYPE_xxxx for current data connection.
@@ -619,6 +622,7 @@ public class TelephonyManager {
      * @see #NETWORK_TYPE_LTE
      * @see #NETWORK_TYPE_EHRPD
      * @see #NETWORK_TYPE_HSPAP
+     * @see #NETWORK_TYPE_TD_SCDMA
      *
      * @hide
      */
@@ -695,6 +699,7 @@ public class TelephonyManager {
             case NETWORK_TYPE_EVDO_B:
             case NETWORK_TYPE_EHRPD:
             case NETWORK_TYPE_HSPAP:
+            case NETWORK_TYPE_TD_SCDMA:
                 return NETWORK_CLASS_3_G;
             case NETWORK_TYPE_LTE:
                 return NETWORK_CLASS_4_G;
@@ -747,6 +752,8 @@ public class TelephonyManager {
                 return "iDEN";
             case NETWORK_TYPE_HSPAP:
                 return "HSPA+";
+            case NETWORK_TYPE_TD_SCDMA:
+                return "TD_SCDMA";
             default:
                 return "UNKNOWN";
         }
