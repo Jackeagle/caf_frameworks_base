4fb9715da8274ce97210365dadf324b718ca1549
Yulian Shandorov <jshandorov@codeaurora.org>
2013-06-26 04:42:54 -0700

    PhoneWindowManager: Add focus key to buttons that don't wake up phone
    Behaviour should be the same as camera key.
    
    Conflicts:
    
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
    
    Change-Id: I158ec843da92d9a2d0c8aaf5d9929a70c9b859ce

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -3993,6 +3993,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case KeyEvent.KEYCODE_MEDIA_RECORD:
             case KeyEvent.KEYCODE_MEDIA_FAST_FORWARD:
             case KeyEvent.KEYCODE_CAMERA:
+            // Same as camera, but pressed before camera key (two level button)
             case KeyEvent.KEYCODE_FOCUS:
                 return false;
         }
