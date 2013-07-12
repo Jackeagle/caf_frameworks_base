5dd0fb2d0361444e714dfc4f1a48302837889e34
vivek mehta <mvivek@codeaurora.org>
2013-07-12 14:46:58 -0700

    frameworks/base: Fix to avoid crash when the tab is not set
    - If current tab is not set from application, getChildTabViewAt
      returns Null value.
      Add a condition to check whether mSelectTab is set to first Tab.
    
    - Tab is not highlighted in Videoapp unlike Music app. Instead
      the list is highlighted with mSelectedTab a negative value
      when onFocusChange is called.
      Add a check for mSelectedTab.
    CRs-Fixed: 509790
    
    Change-Id: I0a7869b9f8198b2145bef5001a246c4b1008b611

--- core/java/android/widget/TabWidget.java
+++ core/java/android/widget/TabWidget.java
@@ -309,9 +309,11 @@ public class TabWidget extends LinearLayout implements OnFocusChangeListener {
 
     @Override
     public void childDrawableStateChanged(View child) {
-        if (getTabCount() > 0 && child == getChildTabViewAt(mSelectedTab)) {
-            // To make sure that the bottom strip is redrawn
-            invalidate();
+        if (mSelectedTab != -1) {
+            if (getTabCount() > 0 && child == getChildTabViewAt(mSelectedTab)) {
+                // To make sure that the bottom strip is redrawn
+                invalidate();
+            }
         }
         super.childDrawableStateChanged(child);
     }
@@ -330,28 +332,30 @@ public class TabWidget extends LinearLayout implements OnFocusChangeListener {
             return;
         }
 
-        final View selectedChild = getChildTabViewAt(mSelectedTab);
+        if(mSelectedTab != -1) {
+           final View selectedChild = getChildTabViewAt(mSelectedTab);
 
-        final Drawable leftStrip = mLeftStrip;
-        final Drawable rightStrip = mRightStrip;
+           final Drawable leftStrip = mLeftStrip;
+           final Drawable rightStrip = mRightStrip;
 
-        leftStrip.setState(selectedChild.getDrawableState());
-        rightStrip.setState(selectedChild.getDrawableState());
+           leftStrip.setState(selectedChild.getDrawableState());
+           rightStrip.setState(selectedChild.getDrawableState());
 
-        if (mStripMoved) {
-            final Rect bounds = mBounds;
-            bounds.left = selectedChild.getLeft();
-            bounds.right = selectedChild.getRight();
-            final int myHeight = getHeight();
-            leftStrip.setBounds(Math.min(0, bounds.left - leftStrip.getIntrinsicWidth()),
-                    myHeight - leftStrip.getIntrinsicHeight(), bounds.left, myHeight);
-            rightStrip.setBounds(bounds.right, myHeight - rightStrip.getIntrinsicHeight(),
-                    Math.max(getWidth(), bounds.right + rightStrip.getIntrinsicWidth()), myHeight);
-            mStripMoved = false;
+           if (mStripMoved) {
+              final Rect bounds = mBounds;
+              bounds.left = selectedChild.getLeft();
+              bounds.right = selectedChild.getRight();
+              final int myHeight = getHeight();
+              leftStrip.setBounds(Math.min(0, bounds.left - leftStrip.getIntrinsicWidth()),
+                      myHeight - leftStrip.getIntrinsicHeight(), bounds.left, myHeight);
+              rightStrip.setBounds(bounds.right, myHeight - rightStrip.getIntrinsicHeight(),
+                      Math.max(getWidth(), bounds.right + rightStrip.getIntrinsicWidth()), myHeight);
+              mStripMoved = false;
+           }
+
+           leftStrip.draw(canvas);
+           rightStrip.draw(canvas);
         }
-
-        leftStrip.draw(canvas);
-        rightStrip.draw(canvas);
     }
 
     /**
@@ -385,7 +389,6 @@ public class TabWidget extends LinearLayout implements OnFocusChangeListener {
         if (index < 0 || index >= getTabCount() || index == mSelectedTab) {
             return;
         }
-
         if (mSelectedTab != -1) {
             getChildTabViewAt(mSelectedTab).setSelected(false);
         }
@@ -510,7 +513,7 @@ public class TabWidget extends LinearLayout implements OnFocusChangeListener {
 
     /** {@inheritDoc} */
     public void onFocusChange(View v, boolean hasFocus) {
-        if (v == this && hasFocus && getTabCount() > 0) {
+        if (v == this && hasFocus && getTabCount() > 0 && ( mSelectedTab!= -1) ) {
             getChildTabViewAt(mSelectedTab).requestFocus();
             return;
         }
