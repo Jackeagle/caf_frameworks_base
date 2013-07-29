972c44d0be6fb82d0d18236fd24b4204afb807dd
kaiyiz <kaiyiz@codeaurora.org>
2013-07-29 12:37:29 +0800

    Actionbar: Soft keyboard can't be revoked by clicking the back button.
    If find dialog displayed, the urlInputView can response
    touch event when click find dialog action bar.
    
    An action bar should always eats touch events.
    
    CRs-Fixed: 411742
    
    Change-Id: Icf75bc63cc4983472e9a04230e50eddab0e28658

--- core/java/com/android/internal/widget/ActionBarContextView.java
+++ core/java/com/android/internal/widget/ActionBarContextView.java
@@ -32,6 +32,7 @@ import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.ActionMode;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.accessibility.AccessibilityEvent;
@@ -537,5 +538,12 @@ public class ActionBarContextView extends AbsActionBarView implements AnimatorLi
 
     public boolean isTitleOptional() {
         return mTitleOptional;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        super.onTouchEvent(ev);
+        // An action bar always eats touch events.
+        return true;
     }
 }
