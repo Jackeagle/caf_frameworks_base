7b39eb000bc3669c136d635a3534af0e8fc7709c
Sunil Dutt <duttus@codeaurora.org>
2013-08-07 12:18:03 +0530

    frameworks/base: Do not enable configured networks on WPS events.
    The frameworks , if enables the configured networks after the WPS
    SUCCESS , the supplicant shall consider the networks based on the
    priority for the WPA2 handshake after the successful WPS handshake.
    This might end up in the station connecting to the AP other than
    the current one with which the WPS handshake is done.Moreover,
    since framework do not disable the networks and the wpa_supplicant
    was the entity that disabled the networks before wps handshake , it
    could actually be cleanest way to re-enable the networks that were
    disabled during the WPS operation within wpa_supplicant.This patch
    addresses this by not enabling the configured networks on WPS events.
    
    CRs-Fixed: 497306
    Change-Id: I1ed5b86f79d245a524e7f87dd2be8493d4898dd7

--- wifi/java/android/net/wifi/WifiStateMachine.java
+++ wifi/java/android/net/wifi/WifiStateMachine.java
@@ -3802,6 +3802,7 @@ public class WifiStateMachine extends StateMachine {
     class WpsRunningState extends State {
         //Tracks the source to provide a reply
         private Message mSourceMessage;
+        private boolean bEnableAllNetworks = true;
         @Override
         public void enter() {
             if (DBG) log(getName() + "\n");
@@ -3817,6 +3818,7 @@ public class WifiStateMachine extends StateMachine {
                     mSourceMessage.recycle();
                     mSourceMessage = null;
                     transitionTo(mDisconnectedState);
+                    bEnableAllNetworks = false;
                     break;
                 case WifiMonitor.WPS_OVERLAP_EVENT:
                     replyToMessage(mSourceMessage, WifiManager.WPS_FAILED,
@@ -3824,6 +3826,7 @@ public class WifiStateMachine extends StateMachine {
                     mSourceMessage.recycle();
                     mSourceMessage = null;
                     transitionTo(mDisconnectedState);
+                    bEnableAllNetworks = false;
                     break;
                 case WifiMonitor.WPS_FAIL_EVENT:
                     //arg1 has the reason for the failure
@@ -3831,6 +3834,7 @@ public class WifiStateMachine extends StateMachine {
                     mSourceMessage.recycle();
                     mSourceMessage = null;
                     transitionTo(mDisconnectedState);
+                    bEnableAllNetworks = false;
                     break;
                 case WifiMonitor.WPS_TIMEOUT_EVENT:
                     replyToMessage(mSourceMessage, WifiManager.WPS_FAILED,
@@ -3838,6 +3842,7 @@ public class WifiStateMachine extends StateMachine {
                     mSourceMessage.recycle();
                     mSourceMessage = null;
                     transitionTo(mDisconnectedState);
+                    bEnableAllNetworks = false;
                     break;
                 case WifiManager.START_WPS:
                     replyToMessage(message, WifiManager.WPS_FAILED, WifiManager.IN_PROGRESS);
@@ -3849,6 +3854,7 @@ public class WifiStateMachine extends StateMachine {
                         replyToMessage(message, WifiManager.CANCEL_WPS_FAILED, WifiManager.ERROR);
                     }
                     transitionTo(mDisconnectedState);
+                    bEnableAllNetworks = false;
                     break;
                 /* Defer all commands that can cause connections to a different network
                  * or put the state machine out of connect mode
@@ -3889,7 +3895,8 @@ public class WifiStateMachine extends StateMachine {
 
         @Override
         public void exit() {
-            mWifiConfigStore.enableAllNetworks();
+            if (bEnableAllNetworks == true)
+                mWifiConfigStore.enableAllNetworks();
             mWifiConfigStore.loadConfiguredNetworks();
         }
     }
