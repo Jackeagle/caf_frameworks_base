db72c4cb4361830404eb2126daa750b58a91d9de
kaiyiz <kaiyiz@codeaurora.org>
2013-08-16 09:40:58 +0800

    Settings: Disable Mobile data after tap OK on network over limit dialog
    After tapping the OK button on the network over limit dialog,
    the "Mobile data" item is not disabled.
    
    Disable the "Mobile data" item after tapping the OK button.
    
    CRs-Fixed: 524765
    
    Change-Id: Id2d36f787aa03e7f44c3e1f0443bc2f61b601571

--- packages/SystemUI/src/com/android/systemui/net/NetworkOverLimitActivity.java
+++ packages/SystemUI/src/com/android/systemui/net/NetworkOverLimitActivity.java
@@ -26,6 +26,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.net.ConnectivityManager;
 import android.net.INetworkPolicyManager;
 import android.net.NetworkPolicy;
 import android.net.NetworkTemplate;
@@ -45,16 +46,24 @@ import com.android.systemui.R;
 public class NetworkOverLimitActivity extends Activity {
     private static final String TAG = "NetworkOverLimitActivity";
 
+    private ConnectivityManager mConnService;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+        mConnService = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
         final NetworkTemplate template = getIntent().getParcelableExtra(EXTRA_NETWORK_TEMPLATE);
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle(getLimitedDialogTitleForTemplate(template));
         builder.setMessage(R.string.data_usage_disabled_dialog);
 
-        builder.setPositiveButton(android.R.string.ok, null);
+        builder.setPositiveButton(
+                android.R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        mConnService.setMobileDataEnabled(false);
+                    }
+                });
         builder.setNegativeButton(
                 R.string.data_usage_disabled_dialog_enable, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
