b6ae6c4b7dc5f678dffa95d5746f6903bd6fa807
Alan Wang <flwang@codeaurora.org>
2013-08-15 19:37:16 -0700

    MSIM: Add support for ro.config.combined_signal
    Add support for ro.config.combined_signal. This property is being used to
    consider data service state for signal display.
    
    When ro.config.combined_signal is not set to false, we will display CS/PS
    signal strength whichever is available.
    
    Change-Id: I422ee79a91377e945c5e951bc0d0663480631d95
    CRs-Fixed: 529594

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MSimNetworkController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MSimNetworkController.java
@@ -401,6 +401,18 @@ public class MSimNetworkController extends NetworkController {
                         + mSubscription + "state=" + state.getState());
                 }
                 mMSimServiceState[mSubscription] = state;
+                if (SystemProperties.getBoolean("ro.config.combined_signal", true)) {
+                    /*
+                     * if combined_signal is set to true only then consider data
+                     * service state for signal display
+                     */
+                    mMSimDataServiceState[mSubscription] =
+                        mMSimServiceState[mSubscription].getDataRegState();
+                    if (DEBUG) {
+                        Slog.d(TAG, "Combining data service state " +
+                                mMSimDataServiceState[mSubscription] + " for signal");
+                    }
+                }
                 updateTelephonySignalStrength(mSubscription);
                 updateDataNetType(mSubscription);
                 updateDataIcon(mSubscription);
