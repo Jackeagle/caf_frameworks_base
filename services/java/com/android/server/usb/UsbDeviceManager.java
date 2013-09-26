94248d2769512aefc250495be97c2adf3921a2c7
Xiaogang Cui <xiaogang@codeaurora.org>
2013-09-26 20:19:25 +0800

    frameworks: UsbDeviceManager: Fix the bug can't switch from UMS to MTP
    When dynamic switch from UMS(USB Mass Storage) to MTP mode, failed to
    disable UMS.
    (1) MountService:When enable UMS and disconnect USB cable. MountService
    will catch this USB_STATE intent, and try to disable UMS.
    (2) StorageNotification: sometimes, MountService will miss this intent.
    because when disconnect, the storage is still on the process of enable
    UMS, but not in Environment.MEDIA_SHARED state. Need StorageNotification
    will catch the final boardcast, and try to disable UMS.
    
    Change-Id: I1189f898d7ef65ee92074b0ab9bfb87e648b5850

--- services/java/com/android/server/usb/UsbDeviceManager.java
+++ services/java/com/android/server/usb/UsbDeviceManager.java
@@ -439,8 +439,7 @@ public class UsbDeviceManager {
                     .equals(new UsbManager(null, null).getDefaultFunction())
                 && !storageManager.isUsbMassStorageEnabled()) {
                 storageManager.enableUsbMassStorage();
-            } else if ((!connected || (connected && isInChargingMode()))
-                && storageManager.isUsbMassStorageEnabled()) {
+            } else if (storageManager.isUsbMassStorageEnabled()) {
                 storageManager.disableUsbMassStorage();
             }
         }
