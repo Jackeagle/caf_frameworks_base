21d28f19017813868fabc635a0510d51102a2926
Susheel nyamala <snyamala@codeaurora.org>
2013-09-27 17:21:30 +0530

    Fix data disconnect issue on G sub for C+G DSDS
    On a DSDS Cdma+Gsm device, when data is registered on both Cdma and Gsm
    subscriptions and DDS is on Gsm sub, if data deregister happens on Cdma
    sub, data disconnect is getting propagated to connectivity service
    irrespective of DDS, hence data is getting disconnected on Gsm sub.
    
    Add code changes to block data disconnect getting propagated to
    connectivity service when the subscription is not DDS.
    
    Change-Id: I329d6a20ace65cad494e36dd9ec5a6470db2fbd7
    CRs-Fixed: 544292

--- services/java/com/android/server/MSimTelephonyRegistry.java
+++ services/java/com/android/server/MSimTelephonyRegistry.java
@@ -318,8 +318,10 @@ class MSimTelephonyRegistry extends ITelephonyRegistryMSim.Stub {
                     }
                     if ((events & PhoneStateListener.LISTEN_DATA_CONNECTION_STATE) != 0) {
                         try {
-                            r.callback.onDataConnectionStateChanged(mDataConnectionState,
-                                mDataConnectionNetworkType);
+                            if (r.subscription == subscription) {
+                                r.callback.onDataConnectionStateChanged(mDataConnectionState,
+                                        mDataConnectionNetworkType);
+                            }
                         } catch (RemoteException ex) {
                             remove(r.binder);
                         }
@@ -533,7 +535,8 @@ class MSimTelephonyRegistry extends ITelephonyRegistryMSim.Stub {
 
     public void notifyDataConnection(int state, boolean isDataConnectivityPossible,
             String reason, String apn, String apnType, LinkProperties linkProperties,
-            LinkCapabilities linkCapabilities, int networkType, boolean roaming) {
+            LinkCapabilities linkCapabilities, int networkType, boolean roaming,
+            int subscription) {
         if (!checkNotifyPermission("notifyDataConnection()" )) {
             return;
         }
@@ -579,8 +582,11 @@ class MSimTelephonyRegistry extends ITelephonyRegistryMSim.Stub {
                         + ", " + mDataConnectionNetworkType + ")");
                 }
                 for (Record r : mRecords) {
-                    if ((r.events & PhoneStateListener.LISTEN_DATA_CONNECTION_STATE) != 0) {
+                    if (((r.events & PhoneStateListener.LISTEN_DATA_CONNECTION_STATE) != 0) &&
+                            (r.subscription == subscription)) {
                         try {
+                            Slog.d(TAG,"Notify data connection state changed on sub: " +
+                                    subscription);
                             r.callback.onDataConnectionStateChanged(mDataConnectionState,
                                     mDataConnectionNetworkType);
                         } catch (RemoteException ex) {
@@ -592,10 +598,11 @@ class MSimTelephonyRegistry extends ITelephonyRegistryMSim.Stub {
             }
         }
         broadcastDataConnectionStateChanged(state, isDataConnectivityPossible, reason, apn,
-                apnType, linkProperties, linkCapabilities, roaming);
+                apnType, linkProperties, linkCapabilities, roaming, subscription);
     }
 
-    public void notifyDataConnectionFailed(String reason, String apnType) {
+    public void notifyDataConnectionFailed(String reason, String apnType,
+            int subscription) {
         if (!checkNotifyPermission("notifyDataConnectionFailed()")) {
             return;
         }
@@ -613,7 +620,7 @@ class MSimTelephonyRegistry extends ITelephonyRegistryMSim.Stub {
             }
         }
         */
-        broadcastDataConnectionFailed(reason, apnType);
+        broadcastDataConnectionFailed(reason, apnType, subscription);
     }
 
     public void notifyCellLocation(Bundle cellLocation, int subscription) {
@@ -756,7 +763,7 @@ class MSimTelephonyRegistry extends ITelephonyRegistryMSim.Stub {
     private void broadcastDataConnectionStateChanged(int state,
             boolean isDataConnectivityPossible,
             String reason, String apn, String apnType, LinkProperties linkProperties,
-            LinkCapabilities linkCapabilities, boolean roaming) {
+            LinkCapabilities linkCapabilities, boolean roaming, int subscription) {
         // Note: not reporting to the battery stats service here, because the
         // status bar takes care of that after taking into account all of the
         // required info.
@@ -783,15 +790,18 @@ class MSimTelephonyRegistry extends ITelephonyRegistryMSim.Stub {
 
         intent.putExtra(PhoneConstants.DATA_APN_KEY, apn);
         intent.putExtra(PhoneConstants.DATA_APN_TYPE_KEY, apnType);
+        intent.putExtra(MSimConstants.SUBSCRIPTION_KEY, subscription);
         mContext.sendStickyBroadcastAsUser(intent, UserHandle.ALL);
     }
 
-    private void broadcastDataConnectionFailed(String reason, String apnType) {
+    private void broadcastDataConnectionFailed(String reason, String apnType,
+            int subscription) {
         Intent intent = new Intent(TelephonyIntents.ACTION_DATA_CONNECTION_FAILED);
         intent.putExtra(PhoneConstants.FAILURE_REASON_KEY, reason);
         intent.putExtra(PhoneConstants.DATA_APN_TYPE_KEY, apnType);
         intent.putExtra(MSimConstants.SUBSCRIPTION_KEY,
                 MSimTelephonyManager.getDefault().getPreferredDataSubscription());
+        intent.putExtra(MSimConstants.SUBSCRIPTION_KEY, subscription);
         mContext.sendStickyBroadcastAsUser(intent, UserHandle.ALL);
     }
 
