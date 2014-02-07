3bcf63c14217852f068b36f31835a9a8ceaf82af
Robert Greenwalt <rgreenwalt@google.com>
2014-02-07 03:52:12 -0800

    DO NOT MERGE Sanitize WifiConfigs
    Do this both on input from apps (giving error) and between wifi and
    ConnectivityService (ignoring bad data).  This means removing all
    addresses beyond the first and all routes but the first default and
    the implied direct-connect routes.
    
    We do this because the user can't monitor the others (no UI), their
    support wasn't intended, they allow redirection of all traffic
    without user knowledge and they allow circumvention of legacy VPNs.
    
    This should not move forward from JB as it breaks IPv6 and K has
    a more resilient VPN.
    
    Bug:12663469
    
    Conflicts:
    	core/java/android/net/LinkProperties.java
    	services/java/com/android/server/WifiService.java
    	wifi/java/android/net/wifi/WifiStateMachine.java
    
    Change-Id: I0d92db7efc30a1bb3e5b8c6e5595bdb9793a16f2

--- wifi/java/android/net/wifi/WifiStateMachine.java
+++ wifi/java/android/net/wifi/WifiStateMachine.java
@@ -1666,10 +1666,12 @@ public class WifiStateMachine extends StateMachine {
     private void configureLinkProperties() {
         if (mWifiConfigStore.isUsingStaticIp(mLastNetworkId)) {
             mLinkProperties = mWifiConfigStore.getLinkProperties(mLastNetworkId);
+            mLinkProperties = WifiConfiguration.stripUndisplayableConfig(mLinkProperties);
         } else {
             synchronized (mDhcpResultsLock) {
                 if ((mDhcpResults != null) && (mDhcpResults.linkProperties != null)) {
-                    mLinkProperties = mDhcpResults.linkProperties;
+                    mLinkProperties = WifiConfiguration.stripUndisplayableConfig(
+                        mDhcpResults.linkProperties);
                 }
             }
             mLinkProperties.setHttpProxy(mWifiConfigStore.getProxyProperties(mLastNetworkId));
@@ -1918,6 +1920,7 @@ public class WifiStateMachine extends StateMachine {
         if (getNetworkDetailedState() == DetailedState.CONNECTED) {
             //DHCP renewal in connected state
             linkProperties.setHttpProxy(mWifiConfigStore.getProxyProperties(mLastNetworkId));
+            linkProperties = WifiConfiguration.stripUndisplayableConfig(linkProperties);
             if (!linkProperties.equals(mLinkProperties)) {
                 if (DBG) {
                     log("Link configuration changed for netId: " + mLastNetworkId
