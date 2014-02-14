8f0f466458ea8e29828fb4800dc484ac6d63112b
Chad Brubaker <cbrubaker@google.com>
2014-02-14 13:22:34 -0800

    Add the calling package name to requestRouteToHost
    The calling package name will be used to check if an application is a
    system application when deciding if a route should be exempt from VPN
    routing rules...
    
    Bug: 12937545
    Change-Id: I2c09c875fe9bb9685871a0a801ddcbb32fc17405

--- core/java/android/net/ConnectivityManager.java
+++ core/java/android/net/ConnectivityManager.java
@@ -390,6 +390,8 @@ public class ConnectivityManager {
 
     private final IConnectivityManager mService;
 
+    private final String mPackageName;
+
     /**
      * Tests if a given integer represents a valid network type.
      * @param networkType the type to be tested
@@ -739,7 +741,7 @@ public class ConnectivityManager {
     public boolean requestRouteToHostAddress(int networkType, InetAddress hostAddress) {
         byte[] address = hostAddress.getAddress();
         try {
-            return mService.requestRouteToHostAddress(networkType, address);
+            return mService.requestRouteToHostAddress(networkType, address, mPackageName);
         } catch (RemoteException e) {
             return false;
         }
@@ -835,8 +837,9 @@ public class ConnectivityManager {
     /**
      * {@hide}
      */
-    public ConnectivityManager(IConnectivityManager service) {
+    public ConnectivityManager(IConnectivityManager service, String packageName) {
         mService = checkNotNull(service, "missing IConnectivityManager");
+        mPackageName = checkNotNull(packageName, "missing package name");
     }
 
     /** {@hide} */
