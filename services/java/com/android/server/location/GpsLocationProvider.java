39ae06dff285da255f2c8dce4b3397d19863c444
Dante Russo <drusso@codeaurora.org>
2014-06-18 09:33:05 -0700

    Use a content observer for getting the default APN
    Instead of executing a query of the database for the
    default APN on every network state change, an observer
    is used on the database to be notifited when the apn
    has changed. This prevents unneccesary queries on the
    the database.
    
    CRs-fixed: 662137
    
    Change-Id: I3155bd8b53a1877413ed54a861df1bfb1853b9e4

--- services/java/com/android/server/location/GpsLocationProvider.java
+++ services/java/com/android/server/location/GpsLocationProvider.java
@@ -24,6 +24,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.Cursor;
+import android.database.ContentObserver;
 import android.hardware.location.GeofenceHardwareImpl;
 import android.hardware.location.IGeofenceHardware;
 import android.location.Criteria;
@@ -319,6 +320,8 @@ public class GpsLocationProvider implements LocationProviderInterface {
     private final ConnectivityManager mConnMgr;
     private final GpsNetInitiatedHandler mNIHandler;
 
+    private String mDefaultApn;
+
     // Wakelocks
     private final static String WAKELOCK_KEY = "GpsLocationProvider";
     private final PowerManager.WakeLock mWakeLock;
@@ -540,6 +543,9 @@ public class GpsLocationProvider implements LocationProviderInterface {
         intentFilter.addAction(ALARM_TIMEOUT);
         intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
         mContext.registerReceiver(mBroadcastReciever, intentFilter, null, mHandler);
+        Uri uri = Uri.parse("content://telephony/carriers/preferapn");
+        mContext.getContentResolver().registerContentObserver(
+                uri, false, mDefaultApnObserver);
     }
 
     /**
@@ -571,14 +577,13 @@ public class GpsLocationProvider implements LocationProviderInterface {
             boolean dataEnabled = Settings.Global.getInt(mContext.getContentResolver(),
                                                          Settings.Global.MOBILE_DATA, 1) == 1;
             boolean networkAvailable = info.isAvailable() && dataEnabled;
-            String defaultApn = getSelectedApn();
-            if (defaultApn == null) {
-                defaultApn = "dummy-apn";
-            }
 
+            if (mDefaultApn == null) {
+                mDefaultApn = getDefaultApn();
+            }
             native_update_network_state(info.isConnected(), info.getType(),
                                         info.isRoaming(), networkAvailable,
-                                        info.getExtraInfo(), defaultApn);
+                                        info.getExtraInfo(), mDefaultApn);
         }
 
         if (info != null && info.getType() == ConnectivityManager.TYPE_MOBILE_SUPL
@@ -1720,6 +1725,14 @@ public class GpsLocationProvider implements LocationProviderInterface {
         }
     };
 
+    ContentObserver mDefaultApnObserver = new ContentObserver(mHandler) {
+        @Override
+        public void onChange(boolean selfChange) {
+            mDefaultApn = getDefaultApn();
+            if (DEBUG) Log.d(TAG, "Observer mDefaultApn=" + mDefaultApn);
+        }
+    };
+
     private final class NetworkLocationListener implements LocationListener {
         @Override
         public void onLocationChanged(Location location) {
@@ -1736,7 +1749,7 @@ public class GpsLocationProvider implements LocationProviderInterface {
         public void onProviderDisabled(String provider) { }
     }
 
-    private String getSelectedApn() {
+    private String getDefaultApn() {
         Uri uri = Uri.parse("content://telephony/carriers/preferapn");
         String apn = null;
 
@@ -1752,6 +1765,11 @@ public class GpsLocationProvider implements LocationProviderInterface {
                 cursor.close();
             }
         }
+
+        if (apn == null) {
+            apn = "dummy-apn";
+        }
+
         return apn;
     }
 
