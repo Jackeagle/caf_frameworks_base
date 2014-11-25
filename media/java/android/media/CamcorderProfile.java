eb194d3eb112bf768f9a45d8a7743f86475b3715
Surajit Podder <spodder@codeaurora.org>
2014-11-25 18:57:28 +0530

    frameworks/base: Add dummy parameters for compatibility
    Added dummy parameters for H265 Encoder profile and 2160p
    camcorder profile for camera app to compile for both
    L and KK
    
    Change-Id: I33e971b396ea0d87d5a694281255da15a35790a6

--- media/java/android/media/CamcorderProfile.java
+++ media/java/android/media/CamcorderProfile.java
@@ -126,6 +126,12 @@ public class CamcorderProfile
 
     public static final int QUALITY_HVGA = 14;
 
+    /**
+    * {@hide}
+    */
+    public static final int QUALITY_2160P = 15;
+
+    // Start and end of quality list
     private static final int QUALITY_LIST_START = QUALITY_LOW;
     private static final int QUALITY_LIST_END = QUALITY_HVGA;
 
