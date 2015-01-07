644ecaf72be0050188bcc4219a812f7ab03747bb
Sravanthi Palakonda <srapal@codeaurora.org>
2015-01-07 13:17:49 +0530

    wifi:Provide UI with SIM info for EAP-SIM authentication
    User Interface requires the SIM information (viz. number / type
    of SIM's supported on the device), using which the respective
    authentication option is provisioned to the user. This commit
    provides the same by querying the information through the API
    exposed by the Wi-Fi framework which further interacts with
    the supplicant. This commit, defines a new class for the
    SIM information which is populated based on the information
    obtained on the query
    
    Change-Id: I5384ee4539aecc6c9c937368857fb92f2d5efa13
    CRs-Fixed: 775942

--- wifi/java/android/net/wifi/WifiConfigStore.java
+++ wifi/java/android/net/wifi/WifiConfigStore.java
@@ -1201,6 +1201,15 @@ class WifiConfigStore {
                 break setVariables;
             }
 
+            if (!mWifiNative.setNetworkVariable(
+                    netId,
+                    WifiConfiguration.SIMNumVarName,
+                    Integer.toString(config.SIMNum))) {
+                loge(config.SIMNum + ": failed to set sim no: "
+                        +config.SIMNum);
+                break setVariables;
+            }
+
             if (config.SSID != null &&
                     !mWifiNative.setNetworkVariable(
                         netId,
@@ -1577,6 +1586,15 @@ class WifiConfigStore {
                 config.priority = Integer.parseInt(value);
             } catch (NumberFormatException ignore) {
             }
+        }
+
+        value = mWifiNative.getNetworkVariable(netId, WifiConfiguration.SIMNumVarName);
+        if (!TextUtils.isEmpty(value)) {
+            try {
+                config.SIMNum = Integer.parseInt(value);
+            } catch (NumberFormatException ignore) {
+              Log.e(TAG,"error in parsing Selected Sim number " + config.SIMNum);
+        }
         }
 
         value = mWifiNative.getNetworkVariable(netId, WifiConfiguration.hiddenSSIDVarName);
