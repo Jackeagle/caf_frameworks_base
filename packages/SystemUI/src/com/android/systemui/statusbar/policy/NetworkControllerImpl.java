f94858d81052a64d6fa216a04e2e28cb01b37aaa
kaiyiz <kaiyiz@codeaurora.org>
2015-03-31 18:07:53 +0800

    SystemUI: Show one name if both plmn and spn are equal
    If both plmn and SPN are equal, lockscreen and status bar
    are shown as duplicate strings like PLMN-PLMN.
    
    In this case, it would be better to show only one string.
    
    Change-Id: I771ceb88868637e1718f3bdd3bbf322b50857cb2
    CRs-Fixed: 812247

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
@@ -1034,6 +1034,8 @@ public class NetworkControllerImpl extends BroadcastReceiver
         }
         StringBuilder str = new StringBuilder();
         boolean something = false;
+        boolean withSamePlmnSpn = showPlmn && plmn != null
+                && showSpn && spn != null && plmn.equals(spn);
         if (showPlmn && plmn != null) {
             if(mContext.getResources().getBoolean(com.android.internal.R.bool.config_display_rat) &&
                     mServiceState != null) {
@@ -1042,7 +1044,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
             str.append(plmn);
             something = true;
         }
-        if (showSpn && spn != null) {
+        if (!withSamePlmnSpn && showSpn && spn != null) {
             if(mContext.getResources().getBoolean(
                     com.android.internal.R.bool.config_spn_display_control)
                     && something){
