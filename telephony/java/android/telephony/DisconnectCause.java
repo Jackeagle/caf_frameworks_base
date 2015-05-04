bcd3f98bcd5ac6e0202d2bd95caaa08433574898
Benergy Meenan Ravuri <bravuri@codeaurora.org>
2015-05-04 23:34:23 +0530

    IMS: New call fail cause for handover not feasible
    When there is a LTE to 3G/2G transition, if handover is
    not feasible, this error code will be propagated to UI
    
    Change-Id: I668ab0c558db6c910ea868d11fe95188fe77165b
    CRs-Fixed: 826223

--- telephony/java/android/telephony/DisconnectCause.java
+++ telephony/java/android/telephony/DisconnectCause.java
@@ -229,6 +229,9 @@ public class DisconnectCause {
     /** EMERGENCY call failed with permanent fail cause */
     public static final int EMERGENCY_PERM_FAILURE         = 93;
 
+    /** call failed due to LTE to 3G/2G handover not feasible */
+    public static final int HO_NOT_FEASIBLE = 98;
+
     //*********************************************************************************************
     // When adding a disconnect type:
     // 1) Please assign the new type the next id value below.
@@ -237,14 +240,14 @@ public class DisconnectCause {
     // 4) Update toString() with the newly added disconnect type.
     // 5) Update android.telecom.DisconnectCauseUtil with any mappings to a telecom.DisconnectCause.
     //
-    // NextId: 92
+    // NextId: 99
     //*********************************************************************************************
 
     /** Smallest valid value for call disconnect codes. */
     public static final int MINIMUM_VALID_VALUE = NOT_DISCONNECTED;
 
     /** Largest valid value for call disconnect codes. */
-    public static final int MAXIMUM_VALID_VALUE = IMS_MERGED_SUCCESSFULLY;
+    public static final int MAXIMUM_VALID_VALUE = HO_NOT_FEASIBLE;
 
     /** Private constructor to avoid class instantiation. */
     private DisconnectCause() {
@@ -350,6 +353,8 @@ public class DisconnectCause {
             return "OUTGOING_CANCELED";
         case IMS_MERGED_SUCCESSFULLY:
             return "IMS_MERGED_SUCCESSFULLY";
+        case HO_NOT_FEASIBLE:
+            return "HO_NOT_FEASIBLE";
         default:
             return "INVALID: " + cause;
         }
