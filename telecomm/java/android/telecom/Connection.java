673f4be9735bc49f7f617f289954e0199aae28b9
Maanasa Gopal <magopa@codeaurora.org>
2015-05-22 14:26:17 +0530

    Define FDN_ENABLED capability
    Define FDN_ENABLED capability
    in Connection and Call.
    
    Change-Id: I73465ddfbe701a14095fcb88ca884404590b3a7f
    CRs-Fixed: 842110

--- telecomm/java/android/telecom/Connection.java
+++ telecomm/java/android/telecom/Connection.java
@@ -194,6 +194,12 @@ public abstract class Connection implements IConferenceable {
     public static final int CALL_TYPE_MODIFIABLE = 0x00020000;
 
     /**
+    * This capability indicates that FDN is enabled.
+    * @hide
+    */
+    public static final int FDN_ENABLED = 0x00040000;
+
+    /**
      * Orientation mode Landscape
      * @hide
      */
