44d08387fce76b20ae7a64419bd0f48030222717
Monika Alekhya <malekh@codeaurora.org>
2015-05-25 20:09:41 +0530

    AlarmManagerService: Check for valid alarm type range
    Alarm type condition is corrected.
    
    Change-Id: Ib750572ac8124fd397bd99fedb49013a451d64c2
    CRs-Fixed: 815682

--- services/core/java/com/android/server/AlarmManagerService.java
+++ services/core/java/com/android/server/AlarmManagerService.java
@@ -746,7 +746,7 @@ class AlarmManagerService extends SystemService {
                     + " seconds");
             interval = MIN_INTERVAL;
         }
-        if (type < RTC_WAKEUP || type > RTC_POWEROFF_WAKEUP || type > ELAPSED_REALTIME) {
+        if (type < RTC_WAKEUP || type > RTC_POWEROFF_WAKEUP) {
             throw new IllegalArgumentException("Invalid alarm type " + type);
         }
 
