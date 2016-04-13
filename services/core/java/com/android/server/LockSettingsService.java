d0f2a5634a686e22cd8c6a8ea5ab52f69cb047ef
Jim Miller <jaggies@google.com>
2016-04-13 16:35:36 -0700

    Fix missing permission check when saving pattern/password
    Fixes bug 28163930
    
    Change-Id: Ic98ef20933b352159b88fdef331e83e9ef6e1f20
    (cherry picked from commit e83f0f6a5a6f35323f5367f99c8e287c440f33f5)

--- services/core/java/com/android/server/LockSettingsService.java
+++ services/core/java/com/android/server/LockSettingsService.java
@@ -449,6 +449,7 @@ public class LockSettingsService extends ILockSettings.Stub {
     @Override
     public void setLockPattern(String pattern, String savedCredential, int userId)
             throws RemoteException {
+        checkWritePermission(userId);
         byte[] currentHandle = getCurrentHandle(userId);
 
         if (pattern == null) {
@@ -477,6 +478,7 @@ public class LockSettingsService extends ILockSettings.Stub {
     @Override
     public void setLockPassword(String password, String savedCredential, int userId)
             throws RemoteException {
+        checkWritePermission(userId);
         byte[] currentHandle = getCurrentHandle(userId);
 
         if (password == null) {
