85838feaea8c8c8d38c4262e74d911e59a275d02
Shreyas Narayan <shrena@codeaurora.org>
2018-02-06 18:02:09 +0530

    Revert "Prevent getting data from Clipboard if device is locked"
    This reverts commit 2574d0980786140de97ebfcbf66dbc5d1b75623a.
    
    Change-Id: I856a9079fe64db0af44383fae1a9a418de959420

--- services/core/java/com/android/server/clipboard/ClipboardService.java
+++ services/core/java/com/android/server/clipboard/ClipboardService.java
@@ -20,7 +20,6 @@ import android.app.ActivityManagerNative;
 import android.app.AppGlobals;
 import android.app.AppOpsManager;
 import android.app.IActivityManager;
-import android.app.KeyguardManager;
 import android.content.BroadcastReceiver;
 import android.content.ClipData;
 import android.content.ClipDescription;
@@ -248,7 +247,7 @@ public class ClipboardService extends IClipboard.Stub {
     public ClipData getPrimaryClip(String pkg) {
         synchronized (this) {
             if (mAppOps.noteOp(AppOpsManager.OP_READ_CLIPBOARD, Binder.getCallingUid(),
-                    pkg) != AppOpsManager.MODE_ALLOWED || isDeviceLocked()) {
+                    pkg) != AppOpsManager.MODE_ALLOWED) {
                 return null;
             }
             addActiveOwnerLocked(Binder.getCallingUid(), pkg);
@@ -259,7 +258,7 @@ public class ClipboardService extends IClipboard.Stub {
     public ClipDescription getPrimaryClipDescription(String callingPackage) {
         synchronized (this) {
             if (mAppOps.checkOp(AppOpsManager.OP_READ_CLIPBOARD, Binder.getCallingUid(),
-                    callingPackage) != AppOpsManager.MODE_ALLOWED || isDeviceLocked()) {
+                    callingPackage) != AppOpsManager.MODE_ALLOWED) {
                 return null;
             }
             PerUserClipboard clipboard = getClipboard();
@@ -270,7 +269,7 @@ public class ClipboardService extends IClipboard.Stub {
     public boolean hasPrimaryClip(String callingPackage) {
         synchronized (this) {
             if (mAppOps.checkOp(AppOpsManager.OP_READ_CLIPBOARD, Binder.getCallingUid(),
-                    callingPackage) != AppOpsManager.MODE_ALLOWED || isDeviceLocked()) {
+                    callingPackage) != AppOpsManager.MODE_ALLOWED) {
                 return false;
             }
             return getClipboard().primaryClip != null;
@@ -294,7 +293,7 @@ public class ClipboardService extends IClipboard.Stub {
     public boolean hasClipboardText(String callingPackage) {
         synchronized (this) {
             if (mAppOps.checkOp(AppOpsManager.OP_READ_CLIPBOARD, Binder.getCallingUid(),
-                    callingPackage) != AppOpsManager.MODE_ALLOWED || isDeviceLocked()) {
+                    callingPackage) != AppOpsManager.MODE_ALLOWED) {
                 return false;
             }
             PerUserClipboard clipboard = getClipboard();
@@ -304,11 +303,6 @@ public class ClipboardService extends IClipboard.Stub {
             }
             return false;
         }
-    }
-
-    private boolean isDeviceLocked() {
-        final KeyguardManager keyguardManager = mContext.getSystemService(KeyguardManager.class);
-        return keyguardManager != null && keyguardManager.isDeviceLocked();
     }
 
     private final void checkUriOwnerLocked(Uri uri, int uid) {
