cf2b96b5a772467c139e3190fc17e72005a4dde5
akirilov <akirilov@google.com>
2018-03-22 14:01:44 -0700

    RESTRICT AUTOMERGE: Prevent reporting fake package name - framework (backport to mnc-dev)
    Test: added AccessibilityEndToEndTest#testPackageNameCannotBeFaked
          cts-tradefed run cts --package android.accessibilityservice
          cts-tradefed run cts --package android.accessibility
    
    BUG:69981755
    
    Change-Id: Id9990ccf1e88cea441f9b8927e849e67a059cfe4
    (cherry picked from commit de71ee469a06fac76710874a69c97f76f4f22af7)

--- core/java/android/view/accessibility/AccessibilityManager.java
+++ core/java/android/view/accessibility/AccessibilityManager.java
@@ -582,7 +582,7 @@ public final class AccessibilityManager {
      * @hide
      */
     public int addAccessibilityInteractionConnection(IWindow windowToken,
-            IAccessibilityInteractionConnection connection) {
+            String packageName, IAccessibilityInteractionConnection connection) {
         final IAccessibilityManager service;
         final int userId;
         synchronized (mLock) {
@@ -593,7 +593,8 @@ public final class AccessibilityManager {
             userId = mUserId;
         }
         try {
-            return service.addAccessibilityInteractionConnection(windowToken, connection, userId);
+            return service.addAccessibilityInteractionConnection(windowToken, connection,
+                    packageName, userId);
         } catch (RemoteException re) {
             Log.e(LOG_TAG, "Error while adding an accessibility interaction connection. ", re);
         }
