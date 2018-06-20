c01a26d571bede7637c0485eb4742df1ccca0649
Shwetha Padmanabha <shwepa@codeaurora.org>
2018-06-20 16:32:17 +0530

    Fix to avoid Signal bar display.
    Voice & Data is in service over IWLAN and cellular Voice is OOS &
    Data is in limited service. since Data is in limited service over
    Cellular network, modem reports signal strength and Voice is in
    service over IWLAN,signal strength is displayed. additionally
    check whether Voice RAT is valid or not to report hasService.
    
    Change-Id: Ib4d569237624870b054b7b62efa8d886bcaa8f23

--- telephony/java/android/telephony/ServiceState.java
+++ telephony/java/android/telephony/ServiceState.java
@@ -1323,6 +1323,7 @@ public class ServiceState implements Parcelable {
 
         // voice overrides
         newSs.mVoiceRegState = voiceSs.mVoiceRegState;
+        newSs.mRilVoiceRadioTechnology = voiceSs.mRilVoiceRadioTechnology;
         newSs.mIsEmergencyOnly = false; // only get here if voice is IN_SERVICE
 
         return newSs;
