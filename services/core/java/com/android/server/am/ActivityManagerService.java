ab492626e1fce477a053008109a7885185d3461d
Karthik Gopalan <gkarth@codeaurora.org>
2018-12-01 19:09:53 +0530

    Revert "Disable swipe boost and preferred apps for certain cases"
    This reverts commit c0cbee2fe77113d04cc1ee6abfb0e0206efc93ef.
    
    CRs-Fixed: 2359219
    
    Change-Id: Ib605a974aa6fceb0e2d260846225dc171f4da065

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -533,7 +533,9 @@ public class ActivityManagerService extends IActivityManager.Stub
     ConnectivityManager mConnectivityManager;
 
     /* Freq Aggr boost objects */
+    public static BoostFramework mPerf = null;
     public static BoostFramework mPerfServiceStartHint = null;
+    public static boolean mIsPerfLockAcquired = false;
     /* UX perf event object */
     public static BoostFramework mUxPerf = new BoostFramework();
 
