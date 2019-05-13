8d6f2d97477a8f87322677e62e645222fa84b0be
wya <wya@codeaurora.org>
2019-05-13 10:10:09 +0800

    Fix the fwr issue due to dead lock
    Change-Id: I6956846a0989293c29e51171120b4da8f2cea8e6

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -635,7 +635,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     int mShortPressOnSleepBehavior;
     int mShortPressOnWindowBehavior;
     volatile boolean mAwake;
-    boolean mScreenOnEarly;
+    volatile boolean mScreenOnEarly;
     boolean mScreenOnFully;
     ScreenOnListener mScreenOnListener;
     boolean mKeyguardDrawComplete;
@@ -7178,9 +7178,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     @Override
     public boolean isScreenOn() {
-        synchronized (mLock) {
-            return mScreenOnEarly;
-        }
+        return mScreenOnEarly;
     }
 
     @Override
